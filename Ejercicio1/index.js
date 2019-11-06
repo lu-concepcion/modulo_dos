46ee746d460fc41d2f47b69b9ddc602a408442b4
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-06 16:05:57 -0300

    Se agregó la solución del ejercicio 1

---
+++ Ejercicio1/index.js
@@ -0,0 +1,9 @@
+// variable edad guardará lo introducido en el prompt
+var numero1 = prompt("Ingrese un numero");
+var numero2 = prompt("Ingrese un numero");
+
+var resultado= parseInt(numero1) + parseInt(numero2);
+
+// levanta una ventana con la edad ingresada
+alert(resultado)
+
