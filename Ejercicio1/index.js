9f65f85ae035bbb260291f4eab15661b06337be6
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-06 16:02:14 -0300

    Se agrega la carpeta con el ejercicio1

---
+++ Ejercicio1/index.js
@@ -0,0 +1,6 @@
+// variable edad guardará lo introducido en el prompt
+var num1 = parceInt(prompt("Introduce numero1 "));
+var num2 = parceInt(prompt("Introduce numero2 "));
+var resul = num1 + num2;
+// levanta una ventana con la edad ingresada
+alert("El resultado es "+resul)
