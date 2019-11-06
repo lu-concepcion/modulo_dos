0d7b76711f7568d919c482ccb76fbf712195f267
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-06 17:32:23 -0300

    Agrega archivos de ejercicio 4 (aún en progreso)

---
+++ Ejercicio4/index.js
@@ -0,0 +1,10 @@
+
+var uno = prompt("Ingresa el Nombre de un Lenguaje de Programación:");
+
+if(uno != "Python")
+for(var i = 0; i<4; i++ )
+{
+    var lenguaje = new Array (prompt("Ingresa otro Nombre de algún Lenguaje de Programación:"));
+    var todos = uno + "-" + lenguaje[i]; 
+}
+ alert(todos)
