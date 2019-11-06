add56f43c2400a512ff5451b0654d2c380193ec6
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-06 16:07:44 -0300

    Se agrego la carpeta: "Ejercicio2", la cual contiene 2 archivos

---
+++ Ejercicio2/index.js
@@ -0,0 +1,14 @@
+function edad()
+{
+	var edad = parseInt(prompt("Ingresa EDAD"));
+
+	if(edad>65)
+	{
+		alert("Felicidades, ya puedes jubilar..no mas AFP!!, pinche karol dance xD");
+	}else
+	{
+		alert("No puedes jubilar")
+	}
+
+	
+}
