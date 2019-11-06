a87c3b9e1a62f02409ac770bc853cf8f30475984
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-06 16:02:43 -0300

    Subir archivos html y js ejercicio 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,7 @@
+var edad = prompt("Introduce tu edad queridimo trabajador");
+if (edad > 65) {
+    var mensaje = "Ya puede jubilar tatita disfrute las vacaciones, recomendamos el parque Ecuador para caminar y haga ejercicio";
+}
+else var mensaje = "Muy joven, estás en la flor de la vida, por ello no hay jubilacion -aún-."
+//levanta una ventana con la edad ingresada
+alert(mensaje)
