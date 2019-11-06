e24949624aa0b3b1eda1032639c7748fa6ad101d
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-06 16:03:57 -0300

    agregué el ejercicio 2 del modulo 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,29 @@
+// variable edad guardará lo introducido en el prompt
+//var num1 = prompt("Introduzca numero 1");
+//var num2 = prompt("introduzca numero 2");
+//var res = parseInt(num1)+parseInt(num2);
+// levanta una ventana con la edad ingresada
+//alert(res)
+
+var edad= prompt("introduzca edad")
+if (parseInt(edad)>=65)
+{
+    alert("felicidades viejito!, ya te puedes ir a florida")
+}
+else 
+{
+    alert("jajaja, en tus sueños")
+}
+
+//var nota = prompt("ingrese la nota");
+//if (parseInt(nota)<=3)
+//{
+ //   alert ("Muy Bajo")
+//}
+//if (parseint(nota)>=4 && parseInt(nota)<=5) {
+  //  alert ("Regular")
+//} 
+//if (parseInt(nota)>=6 && parseInt(nota)<=7)
+//{
+  //  alert("Muy bueno")
+//}
