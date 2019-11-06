e53a3066f826b1160ccdaee8c62c0fba764a774d
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-06 16:06:57 -0300

    Ejercicio 3
    agrego carpeta ejercicio 3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,12 @@
+var nota = prompt("Ingrese una nota");
+if(nota>3){
+    if(nota>5){
+        alert("Muy bueno");
+    }
+    else{
+        alert("Regular");
+    }
+}
+else{
+    alert("Muy bajo");
+}
