48e4bfbe07df55bdb21df39cbb3c9a358e435383
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-06 16:05:13 -0300

    se agrega el ejercicio3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,12 @@
+
+var Calificar = parseInt(prompt("Introduce numero del 1 al 7 "));
+
+if(Calificar <=3 && Calificar >=1){
+    alert(" Muy Baja ");
+}
+if(Calificar <=5 && Calificar >=4){
+    alert(" Regular");
+}
+if(Calificar <=7 && Calificar >=5){
+    alert(" Muy Bueno ");
+}
