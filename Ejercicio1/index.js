88f7efe659fb9010014d02cce6a16d0127edfb95
marcoac001 <marcoac001@lagash.com>
2019-11-06 16:07:21 -0300

    Revert "Se agregó el ejercicio 1"
    This reverts commit e0b3851ba83299cceaeb9fdb4130786202ec54fa.

--- Ejercicio1/index.js
+++
@@ -1,8 +0,0 @@
-// variable edad guardará lo introducido en el prompt
-var n2 = prompt("Ingresa un numero");
-var n1 = prompt("Ingresa tu segundo numero");
-n3 = parseInt(n1) + parseInt(n2);
-
-// levanta una ventana con la edad ingresada
-alert(n3)
-alert("Es tu suma!")
