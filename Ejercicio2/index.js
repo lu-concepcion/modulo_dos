c2571860b3694cfe9e6c8a7897d3b910d45dd871
jeremya-lagash <jeremya@lagash.com>
2019-11-06 16:06:08 -0300

    Se ha añadido el ejercicio2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,7 @@
+var edad = prompt("Introduce tu edad");
+res=parseInt(edad);
+
+if(edad>65){alert("Ya puedes jubilar y recibir la maravillosa pensión que has guardado durante toda una vida de trabajo, ooouh espera... estamos en Chile... Cueeeeeeeeeeeeek")}
+else{alert("aun no puedes jubilar, cuando jubiles podrás recibir tu jugosa pensión")}
+
+
