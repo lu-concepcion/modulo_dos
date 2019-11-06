dcc9f1918f161b7b926c6939886273da38d45b6c
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-06 16:03:44 -0300

    Agregar Ejercicio 1 a branch

---
+++ Ejercicio1/index.js
@@ -0,0 +1,8 @@
+// variable edad guardará lo introducido en el prompt
+var num1 = prompt("Introduzca el primer numero");
+var num2 = prompt("Introduzca el segundo numero");
+
+var suma = (parseInt(num1)+ parseInt(num2));
+
+alert("El resultado de "+num1+ "+" +num2+" = "+ suma);
+
