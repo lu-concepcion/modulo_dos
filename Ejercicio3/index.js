ebdae619ab0111ca8a88701dd97e78c40334c32e
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-06 16:04:03 -0300

    agrega archivos de ejercicio3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,11 @@
+
+var cal = prompt("Ingresa tu calificación:");
+
+if (cal>0 & cal <4)
+alert("Tu calificación es MUY BAJA :( ")
+else if (cal>3 & cal <6)
+alert("Tu calificación es REGULAR :| ")
+else if (cal>5 & cal<8)
+alert("Tu calificación es MUY BUENA :) ")
+else
+alert("Tu calificación no es válida >:( ")
