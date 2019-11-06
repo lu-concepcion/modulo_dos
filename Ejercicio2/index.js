cce73439b7d4ce9e179ddedfde8144c36c9417e2
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-06 16:03:24 -0300

    agrega archivos de ejercicio2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,10 @@
+
+var edad = prompt("Ingresa tu edad:");
+var sexo = prompt("Ingresa 0 si eres hombre o 1 si eres mujer:");
+
+if (edad>65 & sexo == 0)
+alert("¡¡Has ganado unas merecidas vacaciones permanentes!!")
+else if (edad>60 & sexo == 1)
+alert("¡¡Has ganado unas merecidas vacaciones permanentes!!")
+else
+alert("Sigue participando")
