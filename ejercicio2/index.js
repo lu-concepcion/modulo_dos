f7beed4e0ce9490f94a41c267d50d44b456014bc
fabianb-lagash <fabianb@lagash.com>
2019-11-06 16:09:54 -0300

    Se agrega ejercicio 2

---
+++ ejercicio2/index.js
@@ -0,0 +1,11 @@
+var edad = prompt("ingresar su edad")
+var num = parseInt(edad);
+
+if (num > 65) 
+{
+    alert("Felicidades..! ya puede ir a regalonear con los nietos");   
+}
+else
+{
+    alert("Aún hay que trabajar")
+}
