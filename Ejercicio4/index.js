f893cf81ae2f1aba886d671d37b425bedc7369af
ivettef-lagash <56972785+ivettef-lagash@users.noreply.github.com>
2019-11-06 19:57:52 -0300

    Se modifica el código dejándolo funcional

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -4,7 +4,9 @@ var uno = prompt("Ingresa el Nombre de un Lenguaje de Programación:");
 if(uno != "Python")
 for(var i = 0; i<4; i++ )
 {
-    var lenguaje = new Array (prompt("Ingresa otro Nombre de algún Lenguaje de Programación:"));
-    var todos = uno + "-" + lenguaje[i]; 
-}
- alert(todos)
+      lenguaje [i] = prompt ("Ingresa otro Nombre de algún Lenguaje de Programación:");
+        var todos = "-"+lenguaje[0]+"-"+lenguaje[1]+"-"+lenguaje[2]+"-"+lenguaje[3]; 
+        
+    }
+    var suma = uno + todos;
+    alert(suma)
