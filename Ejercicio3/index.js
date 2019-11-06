3734c03591d21fe6cd23ae28718d31f5fc5253d3
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-06 16:07:10 -0300

    Se agregó la solución del ejercicio 3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,15 @@
+var nota = prompt("Ingrese su calificacion");
+
+
+if (parseInt(nota)<=3){
+    alert("Su desempeño fué muy bajo")
+
+} else if (parseInt(nota)<=5){
+    alert("Su desempeño fué regular")
+} else if (parseInt(nota)<=7) {
+    alert("Su desempeño fué muy bueno")
+} else {
+    alert("La nota no es válida")
+}
+
+
