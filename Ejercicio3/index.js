12a32f68f42a9f14ddfa125be70af8ae3fa6525b
melinag <melinag@lagash.com>
2019-11-06 16:06:56 -0300

    añadir ejercicio 3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,14 @@
+//ejercicio 3
+
+var nota = prompt("ingrese calificación de 1 A 7 " )
+
+if (parseInt(nota) <=3 ) {
+    alert("muy bajo");
+}
+else if (parseInt(nota) <=5) {
+    
+ alert("regular"); }
+
+else if (parseInt(nota) <=7) {
+    alert("muy alto");
+}
