e9590cf83df0bf7fabbdaaefecab3a614076b823
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-06 17:31:29 -0300

    agregado ejercicio4 v1

---
+++ Ejercicio4/index.js
@@ -0,0 +1,19 @@
+var mensaje = ""
+//Ejercicio 4
+var contador=0
+//do{
+//    var lp = prompt("Introduce lenguaje de programacion");
+
+//    contador++;
+//}while(contador<4)   
+//alert(lp)
+
+
+while(contador<4 || lp=="Python")
+{
+    var lp = prompt("Introduce lenguaje de programacion");
+    if(contador < 4 && lp=="Python")
+    {
+        
+    }
+}
