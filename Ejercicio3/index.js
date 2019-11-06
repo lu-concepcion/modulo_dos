ccaea59a8654eb08181222beafacec3fb5e74755
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-06 16:02:59 -0300

    agregado ejercicio3

---
+++ Ejercicio3/index.js
@@ -0,0 +1,10 @@
+var nota = prompt("Introduce nota");
+//Ejercicio 3
+
+if(nota >= 1 && nota <=3){
+    alert("Muy Bajo")
+}else if(nota >=4 && nota <=5){
+    alert("Regular")
+}else if(nota >=6 && nota <=7){
+    alert("Muy Bueno")     
+}
