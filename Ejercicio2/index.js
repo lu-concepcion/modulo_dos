bba966f47f13558c1dd6da81128520ceac0c568e
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-06 16:04:37 -0300

    Agregar Ejecicio2 a branch

---
+++ Ejercicio2/index.js
@@ -0,0 +1,11 @@
+// variable edad guardará lo introducido en el prompt
+var edad = prompt("Introduzca su edad");
+
+if ((parseInt(edad) > 65 ) ) {
+    alert("Jubilaste, pero los saiyajines no envejecen")
+}
+
+
+else{
+alert("Aun no jubilas joven padawan")
+}
