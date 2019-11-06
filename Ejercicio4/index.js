6b26d6253fcb66ac4bb82ff494e21bc3385d5f48
bryanm-lagash <bryanm@lagash.com>
2019-11-06 17:30:42 -0300

    Agregar "Ejercicio 4"

---
+++ Ejercicio4/index.js
@@ -0,0 +1,17 @@
+// do{
+//     index = 1;
+//     var item = prompt("Ingrese " + index + "° lenguaje programación :");
+//     index ++;
+
+// }while(index >= 4);
+
+var index = 1;
+for (var i=0; i < index; i++) {
+    var item = prompt("Ingrese " + index + "° lenguaje programación :");
+    index ++;
+    if(item == "Python"){
+        
+        
+    }
+}
+
