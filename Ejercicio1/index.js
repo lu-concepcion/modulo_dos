d1e1021162d32c42588b81882d79575c94944877
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-06 16:05:13 -0300

    Ejercicio 1
    agregue carpeta del ejercicio 1

---
+++ Ejercicio1/index.js
@@ -0,0 +1,6 @@
+//variable edad guardará lo introducido en el prompt
+var n1 = prompt("Introduce el primer numero");
+var n2 = prompt("Introduce el segundo numero");
+var res = parseInt(n1)+ parseInt(n2);
+//levanta una ventana con la edad ingresada
+alert(res)
