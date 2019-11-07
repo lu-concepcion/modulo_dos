62adf6ec2da31c8c0ddd5995dd1a417a2be227d2
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-07 17:04:47 -0300

    Se termino funciolidad del ejercicio 6

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,23 +1,60 @@
 function Numeros()
 {
-	var numero; var contador=0; var n=0;
+	var numero; var contador=0; var n=0; var array=[];
 
-	for(var i=0;i<5;i++)
+	for(var i=1;i<6;i++)
 	{
 
-		numero = parseInt(prompt("ingresa un numero "+1;
+		numero = parseInt(prompt("ingresa un numero "+i));
 
-		if(numero%2!=0)
+		if(numero%2!=0 && numero<2 || numero >20)
 		{
-			alert("Solo admite pares")
+			alert("no es par o esta fuera de rango!!");
+			i--;
+		}else
+		{
+			array[i-1]=numero; 
+
+		}
+
+	/*	if(numero<2 || numero >20)
+		{
+			alert("Solo admite del 2 al 20");
 			i--;
 		}
 
-		//falta xD
-		
+	*/	
+
+
+
 
 
 
 	}
+	var op = prompt("sumar o multiplicar?"); var suma=0; var multiplicar=1;
+
+	if(op=="suma")
+	{
+		for(var i =0;i<5;i++)
+		{
+			suma = suma + array[i];
+
+		}
+
+		alert("la suma es: "+suma);
+	}
+
+	if(op=="multiplica")
+	{
+		for(var i =0;i<5;i++)
+		{
+			multiplicar = multiplicar * array[i];
+
+		}
+
+		alert("la multiplicacion es: "+multiplicar);
+	}
+
+
 
 }
