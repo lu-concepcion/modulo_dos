16b4a8e4de0669a8eccdab2927566a28a0ad1c1a
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-07 17:21:55 -0300

    se establece el limite de la serie

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,2 +1,12 @@
 var limite = 0
 var limite =prompt("introduzca el limite de la serie")
+var serie = []
+if (limite<=15 &&limite>=3)
+for (var i=3;i<=limite;i++)
+    {
+
+    }
+else
+{
+    alert("limite invalido")
+}
