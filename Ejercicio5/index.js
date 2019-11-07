f0c06b29cfd9287d9da1c7dc1b00b9e9670eedc6
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-07 14:58:57 -0300

    creé ejercico 5

---
+++ Ejercicio5/index.js
@@ -0,0 +1,11 @@
+var nombre = prompt("introduzca nombre")
+var edad = prompt("introduzca edad")
+var mensaje = "Salir"
+if (edad>17)
+    alert (Salir)
+    alert (nombre)
+else 
+    {
+     alert(salir)   
+    }
+
