816fd2fdbe2ef3c573aa4baaeeaf6fccf50d9fb3
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-07 18:32:04 -0300

    ejercicio 7  por terminar

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,12 +1,15 @@
-var n;  
-var f;
-var a=0;
-var b=1;
-var c=1
 
-n= parse(prompt("Ingrese numero: "));
-for(i=0;i<n;i++){
-    c=b+1;
+var f=[0,1];
+
+var numero = parseInt(prompt("Ingrese numero: "));
+
+
+for(var i=2;i<numero;i++){
+    
+    f[i]=f[i-1]+f[i-2];
 
    
+   
 }
+document.write(f);
+
