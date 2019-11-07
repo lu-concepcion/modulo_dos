bad8a92e313efaed193e5dac4a65420edb229ffb
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-07 16:30:00 -0300

    agregado ejercicio6 v1

---
+++ Ejercicio6/index.js
@@ -0,0 +1,17 @@
+//Escribir un algoritmo que solicite números pares, si se ingresa un número impar se debe volver a solicitar otro número.
+//Debe aceptar 5 números pares (positivos y comenzando del 2 y culminando en el 20, ambos inclusive)
+//Posterior a esto se debe solicitar que tipo de operación quiere realizar con los números:
+//Sumar: suma cada uno de los números y entrega el resultado
+//Multiplicar: multiplica cada uno de los números y entrega el resultado
+
+var mayor = 0
+var nombre_mayor=""
+//Ejercicio 5
+var contador=0
+var numerosPares = [];
+var numero = prompt("Introduce numero");
+if(numero%2==0 && numero>=2 && numero<=20){
+    numerosPares[contador]= numero;
+    contador++;
+}
+document.write(numerosPares)
