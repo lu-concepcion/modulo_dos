e1cd9bcf0a1ae56b99fa7a781f1b8d0263b687e7
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-07 14:55:49 -0300

    Revert "Revert "agregue el ejercicio 4 sin terminar""
    This reverts commit 67fda70e1d5d9a23f02508073618be3138a1557b.

---
+++ Ejercicio4/index.js
@@ -0,0 +1,9 @@
+// variable edad guardará lo introducido en el prompt
+var Lenguaje =prompt("Introduce un lenguaje de programacion: ");
+pro ="";
+contador = 0;
+if(Lenguaje1 = "Python" || contador < 4)
+pro = lenguaje1+"-"+lenguaje1;
+    
+contador++;
+
