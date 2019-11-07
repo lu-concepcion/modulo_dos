56ed353d9a0a2df3a1ac81efba7ad3e88a620610
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-07 16:48:30 -0300

    agregar ejercicio6 completo terminado

--- Ejercicio 6/index.js
+++ Ejercicio 6/index.js
@@ -1,9 +1,13 @@
 var numero;
+var operacion;
+var suma=0;
+var multiplicacion=1;
 
 for(var i=1;i<=5;i++){
-numero = parseInt(prompt("Ingrese numero Par: "+(i+1)));
+numero = parseInt(prompt("Ingrese numero Par: "+(i)));
 if (numero%2==0){
-
+ suma = suma + numero;
+ multiplicacion = multiplicacion * numero;
 }
 else{
     alert("No Puedes Ingresar numeros Impar")
@@ -19,3 +23,12 @@ if(numero >= 2 && numero <=20){
 }
 
 }
+operacion= prompt("Que operacion decea ingresar suma o multiplicacion: ");
+if(operacion == "suma"){
+    alert(suma);
+}else{
+    if(operacion== "multiplicacion")
+   alert( multiplicacion);
+}
+
+
