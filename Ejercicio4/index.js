8322693462260219c9f65554ba8e8516ac674162
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-07 14:56:57 -0300

    Agregar Ejercicio4

---
+++ Ejercicio4/index.js
@@ -0,0 +1,20 @@
+// variable edad guardará lo introducido en el prompt
+contador =0;
+var pro="";
+for(i=0;i<4;i++){
+    if(parseInt(contador<4)){
+  var Lenguaje= prompt("Introduce un Lenguaje : ");  
+  contador++;
+    
+    
+        
+    }
+    else {alert("finalizar");
+}
+}
+
+
+
+
+ 
+
