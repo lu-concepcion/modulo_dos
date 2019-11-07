566b49bb862287800d92ad2d7ac8c12e5841bbd1
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-07 17:13:54 -0300

    genera solicitud de cifras para la serie

---
+++ Ejercicio7/index.js
@@ -0,0 +1,6 @@
+var cifras = prompt("Ingrese una cifra entre 3 y 15 para generar la serie Fibonacci");
+
+if(cifras <3 || cifras>15)
+{
+cifras = prompt("Número inválido, ingrese una cifra entre 3 y 15 para generar la serie Fibonacci");
+}
