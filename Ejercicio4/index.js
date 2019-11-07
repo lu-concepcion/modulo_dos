05c9705c3804b0310eec8457f33c253ee9745da5
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-07 12:50:09 -0300

    Añadido ejercicio4 terminado

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -1,19 +1,23 @@
 var mensaje = ""
 //Ejercicio 4
 var contador=0
-//do{
+do{
+    var lenguaje_program = prompt("Introduce lenguaje de programacion");
+    contador++;
+    if(lenguaje_program == "Python" && contador>4){
+        mensaje = mensaje + "-" + lenguaje_program
+    }else{if(lenguaje_program == "Python" && contador <4)
+    {mensaje = mensaje}
+    else{mensaje = mensaje + "-"+ lenguaje_program}}
+}while(contador<4 || lenguaje_program !="Python")   
+alert(mensaje)
+
+
+//while(contador<4 || lp!="Python")
+//{
 //    var lp = prompt("Introduce lenguaje de programacion");
-
-//    contador++;
-//}while(contador<4)   
-//alert(lp)
-
-
-while(contador<4 || lp=="Python")
-{
-    var lp = prompt("Introduce lenguaje de programacion");
-    if(contador < 4 && lp=="Python")
-    {
+//    if(contador < 4 && lp=="Python")
+//    {
         
-    }
-}
+//    }
+//}
