ca59f84bb57562ae2f892458360c4cc697f8d61e
jeremya-lagash <jeremya@lagash.com>
2019-11-07 17:46:04 -0300

    se cambio el ciclo inicial por do while y se agrregaron variables de inicializacion

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,6 +1,11 @@
 var Nsucesiones = prompt("ingrese las suceciones de Fibonacci que desea")
-if(Nsucesiones>=3 && Nsucesiones<=15){
+fibonacci=[0,1];
+num1, num2 =1;
+do{
     for(i=0; i<Nsucesiones; i++){
-        
+        resultado=num1+num2;
+        fibonacci.push(resultado);
+        num1=resultado;
+        num2=num1;
     }
-}
+}while(Nsucesiones>=3 && Nsucesiones<=15)
