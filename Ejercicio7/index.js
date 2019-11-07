9f1262436d8b9a1d3ca26f2da7d99c048272645b
jeremya-lagash <jeremya@lagash.com>
2019-11-07 18:12:25 -0300

    Ejercicio7 resuelto por Hernan :)

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,11 +1,8 @@
-var Nsucesiones = prompt("ingrese las suceciones de Fibonacci que desea")
 fibonacci=[0,1];
-num1, num2 =1;
 do{
-    for(i=0; i<Nsucesiones; i++){
-        resultado=num1+num2;
-        fibonacci.push(resultado);
-        num1=resultado;
-        num2=num1;
-    }
-}while(Nsucesiones>=3 && Nsucesiones<=15)
+    var Nsucesiones = prompt("ingrese las suceciones de Fibonacci que desea")
+}while(Nsucesiones<=3 && Nsucesiones>=15)
+for(i=2; i<Nsucesiones; i++){
+    fibonacci[i]=parseInt(fibonacci[i-1])+parseInt(fibonacci[i-2]);
+}
+document.write(fibonacci)
