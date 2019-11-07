e1be9c6e3f76794dc5087cbb89ea843aa7362f3e
marcoac001 <marcoac001@lagash.com>
2019-11-07 17:13:34 -0300

    Agregué las variables declaradas a mi ciclo for

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,7 +1,9 @@
-variable_auxiliar = 0;
+variable_auxiliar = 1;
 variable_suma= 0;
-fibonacci = "";
+fibonacci = 0;
 var limite_de_secuencia = prompt ("¿Hasta que numero se hará la secuencia?");
 for(i=0;i<n;i++){
-
+    variable_auxiliar= parseInt(variable_auxiliar) + parseInt(variable_suma);
+    variable_suma=variable_auxiliar
+    fibonacci = fibonacci +", "+ variable_auxiliar; 
 }
