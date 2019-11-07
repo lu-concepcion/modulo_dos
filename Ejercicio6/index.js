a6ba73f8279774f0a2a7a87cfc30e73e92b8c419
estebanl-lagash <estebanl@lagash.com>
2019-11-07 16:43:56 -0300

    avance ejercicio 6

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,19 +1,22 @@
 
 numpar = 0;
-
+var num = [2,4,6,8,10];
 
 do{
 
+
 var ns = prompt ("Ingrese un numero par");
 
-
-if(ns%2 == 0){
-
-numpar++
-
-}else{ if (ns<2 || ns>20){
+if (ns<2 || ns>20){
 
   alert ("El numero a ingresar debe ser entre 2 a 20")
+
+}else{ 
+  
+  if(ns%2 == 0){
+  
+  num[numpar] = parseInt (numpar);  
+  numpar++
 
 }else {
 
@@ -21,6 +24,10 @@ numpar++
 
 }
 }
-
-
 }while(numpar <= 5)
+
+if (ope == "multiplicar"){
+
+}else{
+  
+}
