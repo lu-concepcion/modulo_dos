5c768124dce9314a744512002d43abf342237d60
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-07 17:07:25 -0300

    ejercicio6 terminado difinitivamente

--- Ejercicio 6/index.js
+++ Ejercicio 6/index.js
@@ -3,23 +3,15 @@ var operacion;
 var suma=0;
 var multiplicacion=1;
 
-for(var i=1;i<=5;i++){
+for(var i=0;i<5;i++){
 numero = parseInt(prompt("Ingrese numero Par: "+(i)));
-if (numero%2==0){
+if (numero%2==0 && (numero>=2 && numero <=20)){
  suma = suma + numero;
  multiplicacion = multiplicacion * numero;
 }
 else{
-    alert("No Puedes Ingresar numeros Impar")
+    alert("No Puedes Ingresar numeros Impar o numeros fuera de rango")
     i--;
-}
-
-if(numero >= 2 && numero <=20){
-
-}else{
-    alert("No puede agregar numeros fuera de rango");
-    i--;
-
 }
 
 }
