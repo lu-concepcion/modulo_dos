89949275f22c9dc3826cc9f660cfda2f9a0d8611
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-07 15:39:17 -0300

    ejercico 5 funcionando

--- Ejercicio5/index.js
+++ Ejercicio5/index.js
@@ -1,11 +1,27 @@
-var nombre = prompt("introduzca nombre")
-var edad = prompt("introduzca edad")
-var mensaje = "Salir"
-if (edad>17)
-    alert (Salir)
-    alert (nombre)
-else 
+var cont = 0
+var nombre = ""
+var cadena = ""
+var edad = 0
+var edadant = 0
+var separacion = " - "
+do
+{
+    nombre = prompt("ingrese un nombre")
+    if (nombre!="salir")
     {
-     alert(salir)   
+        edad = prompt("ingrese edad")
+        cont++
+        if (edad>edadant)
+        {
+            cadena=nombre
+            edadant=edad
+        }
     }
-
+    if ((nombre =="salir")&&(cont>4))
+    {
+        break
+    }
+    var exit=((cont<4)||(nombre!="salir"))
+}
+while(exit)
+alert (cadena)
