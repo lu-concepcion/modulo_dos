05def1da8caed2c46f88a37566c5f74bd6af03f6
jeremya-lagash <jeremya@lagash.com>
2019-11-07 16:29:02 -0300

    Se ha agregado el ejercicio6 muy incompleto

---
+++ Ejercicio6/index.js
@@ -0,0 +1,29 @@
+var numerosPares = [];
+var numerosIngresados = [];
+for(var i=0; i=5; i++){
+    
+}
+
+
+
+
+
+var contador = 0;
+var edadmayor = 0;
+var nombremayor;
+do{
+    var nombre = prompt("Introduce un Nombre");
+    if(contador > 3){
+        if(nombre=="Salir"){
+            break;
+        }
+    }else{
+        var edad = prompt("Introduce su Edad");
+        if(edad>edadmayor){
+            edadmayor=edad;
+            nombremayor=nombre;
+        }
+    }
+    contador++;
+}while (nombre != "Salir" || edad != "Salir" && contador < 4)
+alert(nombremayor + " es la persona mayor con " + edadmayor)
