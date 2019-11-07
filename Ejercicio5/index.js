63f0b3d904e52b6521ea85fe7c08f7824379ad1f
marcoac001 <marcoac001@lagash.com>
2019-11-07 15:45:58 -0300

    Update index.js

--- Ejercicio5/index.js
+++ Ejercicio5/index.js
@@ -11,6 +11,7 @@ if(guardar_edad=="Salir"||guardar_edad=="salir"&&parseInt(contador)>=4){
     flag= 0;
     alert(nombre +" es el mayor con "+edad +" años");
 }else{
+    //SE SUMA EL CONTADOR Y LUEGO PIDE EL NOMBRE
     contador++;
     var guardar_nombre = prompt("Ingresa el nombre completo");
 //VERIFICA SI SE INGRESA SALIR, SI ES ASI VERIFICA SI EL CONTADOR ES 4 TAMBIEN
@@ -18,8 +19,9 @@ if(guardar_edad=="Salir"||guardar_edad=="salir"&&parseInt(contador)>=4){
     flag= 0;
     alert(nombre +" es el mayor con "+edad +" años")
 }else{
+    //SE VERIFICA SI ES MAYOR QUE EL ANTERIOR GUARDADO
     if(parseInt(guardar_edad)>edad){ 
-        //SE GUARDAN EN LAS VARIBLES AUXILIARES EL NOMBRE Y LA EDAD  
+        //SE GUARDAN LOS NOMBRES Y EDADES EN LAS VARIBLES AUXILIARES DEL NOMBRE Y LA EDAD  
     edad = guardar_edad;
     nombre = guardar_nombre;
     }
