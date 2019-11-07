b938966a93f472c7c4f3d41e612835f590b7d001
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-07 16:30:02 -0300

    archivos de ejercicio 6 (en progreso)

---
+++ Ejercicio6/index.js
@@ -0,0 +1,46 @@
+// var nombre = [];
+// var edad = []; 
+
+//  for(var i = 0; i<4; i++)
+//  {
+//     nombre[i] = prompt("Ingrese su nombre");
+//     edad[i] = prompt("Ingrese su edad");
+//  }
+ 
+//  var salir = prompt("Escriba Salir para finalizar");
+
+//  if(salir == "Salir")
+
+//     for(var j = 0; j<4; j++)
+//     {
+//         if (edad[j]<edad[j+1])
+
+//             var salida = nombre[j+1];
+//     }
+//     alert(salida)
+var numero = [];
+var numeropar = [];
+var suma = 0;
+var multiplicacion = 0;
+
+ for(var i = 0; i<5; i++)
+ {
+    numero[i] = prompt("Ingrese número par entre 0 y 20");
+    if(numero [i] % 2!= 0 ||numero [i] <0 || numero [i]>20)
+    {
+      numero [i] = prompt("Número inválido, ingrese nuevamente");
+      if (numero [i] % 2== 0 && numero [i] >0 && numero [i]<21)
+      {
+         numeropar[i] = numero[i];
+      }
+    }
+      numeropar[i] = numero[i];
+    var pregunta = prompt("Ingrese S para sumar los números ingresados o M para multiplicarlos");
+    
+    if (pregunta == "S")
+    {
+       suma = cero+ numeropar[i]
+    }
+    
+ }
+alert(numeropar)
