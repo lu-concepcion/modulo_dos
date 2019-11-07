d50ae966dc23c4ede9e917c71a9418e0e724d2a8
bryanm-lagash <bryanm@lagash.com>
2019-11-07 17:39:47 -0300

    Agregar logica de Fibonacci

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,6 +1,11 @@
+var primero = 0;
+var segundo = 1;
+var sum = primero + "," + segundo + ","; 
 var cifra = prompt("Ingrese numero de suceciones(min: 3, max:15): ");
 if(cifra >= 3 && cifra <= 15){
     for(var i=0; i < cifra; i++){
-
+        sum = parseInt(primero) + parseInt(segund)
+        primero = segundo;
+        segundo = sum;
     }
 }
