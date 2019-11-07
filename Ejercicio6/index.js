be200cb43dcfed39baf65c2ac184332f504af5a5
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-07 16:30:47 -0300

    Se agrego carpeta "Ejercicio6"

---
+++ Ejercicio6/index.js
@@ -0,0 +1,23 @@
+function Numeros()
+{
+	var numero; var contador=0; var n=0;
+
+	for(var i=0;i<5;i++)
+	{
+
+		numero = parseInt(prompt("ingresa un numero "+1;
+
+		if(numero%2!=0)
+		{
+			alert("Solo admite pares")
+			i--;
+		}
+
+		//falta xD
+		
+
+
+
+	}
+
+}
