ee8fd18b84b3556ac5466b3e0af3df0213eded46
bryanm-lagash <bryanm@lagash.com>
2019-11-07 17:55:49 -0300

    Ejercicio completado

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,11 +1,17 @@
 var primero = 0;
 var segundo = 1;
-var sum = primero + "," + segundo + ","; 
+var list = primero + "," + segundo; 
+var sum;
+
 var cifra = prompt("Ingrese numero de suceciones(min: 3, max:15): ");
 if(cifra >= 3 && cifra <= 15){
+   
     for(var i=0; i < cifra; i++){
-        sum = parseInt(primero) + parseInt(segund)
+        sum = parseInt(primero) + parseInt(segundo);
         primero = segundo;
         segundo = sum;
-    }
+        list = list + "," + sum;
+    } 
 }
+
+alert(list);
