6c188b383484897aadb0b254637d942918034860
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-07 17:13:42 -0300

    validacion del numero n entre 3 y 15
    pregunta por un numero hasta que se cumpla la condicion

---
+++ Ejercicio7/index.js
@@ -0,0 +1,12 @@
+// Escribir un algoritmo que genere la secuencia Fibonacci de las primeras N sucesiones, considerando el 0 y 1
+// que dan inicio a esta sucesión. Para N se debe considerar un mínimo de 3 y un máximo de 15 y se debe 
+//solicitar por pantalla vía prompt hasta que se ingrese un valor válido.
+// Se debe utilizar un ciclo for para recorrer las N sucesiones.
+// El resultado debe ir en el siguiente formato: 0, 1, 1, 2, 3, 5 … y debe ser mostrado en el navegador, 
+//para ello usar: document.write()
+// Fibonacci es una sucesión de números naturales, comenzando del 0 y 1, luego de estos cada número que 
+//continúa es la suma de los dos anteriores, 
+// Ej: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34….
+do{
+    var n = prompt("Ingrese un numero entre 3 y 15");
+}while(n>=3 && n<15);
