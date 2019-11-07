01291fb786bfc4ea2886b8937dcc0722ae2efd1d
bryanm-lagash <bryanm@lagash.com>
2019-11-07 17:20:42 -0300

    Agregar ciclo for para secuencia

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,7 +1,6 @@
 var cifra = prompt("Ingrese numero de suceciones(min: 3, max:15): ");
 if(cifra >= 3 && cifra <= 15){
-    alert("Numero correcto");
-}
-else {
-    alert("Numero incorrecto");
+    for(var i=0; i < cifra; i++){
+
+    }
 }
