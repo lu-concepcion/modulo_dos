19b2299009d5e52f3ff3f009984d73470bafef7e
melinag <melinag@lagash.com>
2019-11-07 16:31:20 -0300

    se añade avance ejercicio 6

---
+++ Ejercicio6/index.js
@@ -0,0 +1,14 @@
+var contador = 0;
+var numerosPares = [];
+
+do {
+    var p = prompt("ingrese un numero par");
+    
+    if ( p %2 == 0 ){
+       numerosPares.push(p)
+    }
+      
+    else {
+       break;
+    }
+    }while();
