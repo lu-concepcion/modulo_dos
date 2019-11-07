c4543d44a6b3d91defcf20e3d91d002bd270bb17
estebanl-lagash <estebanl@lagash.com>
2019-11-07 15:56:05 -0300

    Avance ejercicio 5

---
+++ Ejercicio5/index.js
@@ -0,0 +1,25 @@
+
+contador = 1;
+
+do {
+
+  var sobnombre = prompt ("Ingrese un Nombre");
+
+  if (nombre == salir && contador >= 4){
+
+    alert ("la persona con la mayor edad ingresada es "+sobnombre)
+
+  }else{
+  var edad = prompt ("Ingrese su edad") 
+  
+  if (parseInt(sobedad)>parseInt(edad)) {
+   sobnombre = nombre  
+   sobedad = edad
+  }
+}
+
+
+ contador++;
+
+}while(contador == 4)
+
