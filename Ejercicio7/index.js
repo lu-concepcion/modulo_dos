ff38dd65c8257470cb4e8a0d4061a7b4ce8458b5
estebanl-lagash <estebanl@lagash.com>
2019-11-07 17:16:24 -0300

    actualizacion ejercicio 7 - Primera parte

---
+++ Ejercicio7/index.js
@@ -0,0 +1,15 @@
+cont = 0
+var tam = prompt ("Ingrese el tamaño de muestra");
+
+do{
+if (tam<3 || tam>15){
+
+    alert ("El tamaño de la muestra debe de estar entre 3 a 15");
+
+}else{
+
+    
+
+}
+}
+while(cont == tam)
