02c876bd1017be66fc72e6574dff84b871c198f1
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-07 17:29:04 -0300

    creadas Variables y ciclo for para calculo de fibonacci

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,12 +1,27 @@
 
 
 
-
+var primero = 0;
+var segundo = 1;
 
 do {
     var n = prompt("Indique cuantos numeros desea incluir en su secuencia de Fibonacci");
 
 } while (n < 3 || n > 15);
+
+
+for (let i = 2; i < n; i++) {
+
+     var sumatoria = parseInt(primero) + parseInt(segundo);
+     primero= segundo;
+     segundo = sumatoria;
+     
+    
+}
+alert(sumatoria);
+
+
+
 
 
 
