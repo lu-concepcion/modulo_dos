6e4c991aa4a12bf9ff8b459f474a1a631184ce98
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-07 18:30:11 -0300

    Se completó la funcionalidad del ejercicio

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,6 +1,6 @@
 function Fibonacci()
 {
-	var error="false";
+	var error="false"; var a=0; var b=1; f=[0,1];
 
 	do
 	{
@@ -14,13 +14,17 @@ function Fibonacci()
 
 	    }
 
-	   
-
-
-
 	}while(error=="true");
 
+	//document.write(a,b);
 
+	for(var i=2;i<numero;i++)
+	{
+		f[i]=f[i-1]+f[i-2];
+
+	}
+
+	document.write(f);
 
 	
 
