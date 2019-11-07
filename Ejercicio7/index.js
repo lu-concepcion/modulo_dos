dd1d6d01186168f8bf0e3464d7af65e11d477372
nicolasar1 <nicolasar@lagash.com>
2019-11-07 17:44:17 -0300

    Ejercicio 7 realmente terminado
    se corrige do while y se arregla el tamaño del F a mostrar

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,13 +1,13 @@
 do{
     var size = prompt("Cantidad de la sucesión");
-}while(size < 2 || size > 16);
+}while(size < 3 || size > 15);
 
 var nPrimero = 0;
 var nSegundo = 1;
 var nTotal = nPrimero+nSegundo;
 var secuencia = "0 " + "1 ";
 
-for(var i=0; i<=size; i++){
+for(var i=0; i<=size-3; i++){
     nSegundo = nPrimero;
     nPrimero = nTotal;
     nTotal = nPrimero+nSegundo;
