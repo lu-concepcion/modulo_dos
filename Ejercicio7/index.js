bf38090d2b1f7580b39e935a92b2d009db842359
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-07 17:29:04 -0300

    construccion del for para el calculo de la serie

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -2,9 +2,13 @@ var limite = 0
 var limite =prompt("introduzca el limite de la serie")
 var serie = []
 if (limite<=15 &&limite>=3)
+serie[0]=0
+serie[1]=1
+serie[2]=1
 for (var i=3;i<=limite;i++)
-    {
-
+    {   
+        var j=i--
+        serie[i]=parseInt(serie[j]+serie[j--])
     }
 else
 {
