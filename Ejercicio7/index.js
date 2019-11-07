2c4286f3ff14148afe111f08e219fdd296fb0b32
nicolasar1 <nicolasar@lagash.com>
2019-11-07 17:38:25 -0300

    Ejercicio 7 terminado
    Se agrega lógica al for previamente habiendo definido variables

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -2,10 +2,16 @@ do{
     var size = prompt("Cantidad de la sucesión");
 }while(size < 2 || size > 16);
 
-for(var i=0; i<size; i++){
+var nPrimero = 0;
+var nSegundo = 1;
+var nTotal = nPrimero+nSegundo;
+var secuencia = "0 " + "1 ";
 
+for(var i=0; i<=size; i++){
+    nSegundo = nPrimero;
+    nPrimero = nTotal;
+    nTotal = nPrimero+nSegundo;
+    secuencia = secuencia + nTotal + " ";
 }
-
-var secuencia = "";
 
 document.write(secuencia);
