e7ee797f40a8f5fb31f09fb7c1cce51700832c2e
melinag <melinag@lagash.com>
2019-11-07 16:45:27 -0300

    se añade avance ejercicio 6

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -4,11 +4,11 @@ var numerosPares = [];
 do {
     var p = prompt("ingrese un numero par");
     
-    if ( p %2 == 0 ){
+    if ( p %2 == 0 && p>=2 && p<=20){
        numerosPares.push(p)
     }
-      
-    else {
-       break;
-    }
-    }while();
+    }while(numerosPares.length < 5);
+
+do {
+    
+}
