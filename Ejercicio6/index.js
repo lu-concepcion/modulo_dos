de69f9a5bc2c4253e6910e25bf17a669970ae93b
bryanm-lagash <bryanm@lagash.com>
2019-11-07 16:40:30 -0300

    Completando parte de "Ejercicio 6"

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,11 +1,17 @@
+var numeros = [];
 var numero = [];
 
 for(var i=0; i < 5; i++){
-    numero[i] = prompt("Ingrese numero: ")
-    if(numero % 2 == 0 ){
+    numero[i] = prompt("Ingrese numero par: ");
+    if(numero[i] % 2 == 0 || numero[i] < 2 || numero[i] > 20){
         alert("Par");
+        numeros.push(numero[i]);
     }
     else{
-        alert("Impar")
+        numero[i] = prompt("Numero incorreto, ingrese nuevamente: ");
+        if(numero[i] % 2 == 0 ){
+
+        }
     }
+    
 }
