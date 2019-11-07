8919fc50542bc0b7cc9f80297dbab00dddecb6be
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-07 17:16:19 -0300

    Implementacion ciclo for para la secuencia

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,3 +1,14 @@
 do {
     var secuencia = prompt("Ingresar el largo de la secuencia")    
 } while ((parseInt(secuencia) < 3)&&(parseInt(secuencia) > 15));
+var num1 = 0;
+var num2 = 1;
+var num3 = 0;
+var fibonacci = "0, 1";
+for (var i = 1; i < secuencia; i++) {
+    num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
+    fibonacci = fibonacci + ", " + num3;
+}
+alert(fibonacci)
