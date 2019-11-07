cb723cc8acf47ef80c41344a9a62c1c7e8dfaff0
felipep-lagash <felipep@lagash.com>
2019-11-07 17:15:00 -0300

    Se creó validación para asegurar rango de números

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,6 +1,17 @@
 let sucesiones = 0;
+let anterior = 0;
+let cadenaCompleta = '';
 
- sucesiones = prompt('Ingresa el número de secesiones (entre 2 y 15)');
+while(true){
+    sucesiones = prompt('Ingresa el número de secesiones (entre 2 y 15)');
+    if (sucesiones < 3 ||  sucesiones > 15) {
+        alert('Error: Ingresa solo números entre 3 y 15')
+    } else {
+        break;
+    }
+}
+
+sucesiones = prompt('Ingresa el número de secesiones (entre 2 y 15)');
 
  for (let i = 0; i < sucesiones; i++) {
      
