226ead3aa7755798ca60b4d3ec5e6cabbff215c6
felipep-lagash <felipep@lagash.com>
2019-11-08 15:54:25 -0300

    Se creó objeto vahículo y estructura para mostrar información en pantalla

---
+++ Ejercicio9/index.js
@@ -0,0 +1,17 @@
+let marca = ""
+let modelo = ""
+let anio = ""
+let color = ""
+
+var vahiculo = new Object();
+
+vahiculo.marca = marca;
+vahiculo.modelo = modelo;
+vahiculo.anio = anio;
+vahiculo.color = color;
+
+document.write(vahiculo.marca + ', ');
+document.write(vahiculo.modelo + ', ');
+document.write(vahiculo.anio + ', ');
+document.write(vahiculo.color);
+
