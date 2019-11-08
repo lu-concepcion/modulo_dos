373a075e6b0006343e4b3a78807a99b93a0275db
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-08 15:10:43 -0300

    se creo el metedo DatosPersona

---
+++ Ejercicio8/index.js
@@ -0,0 +1,19 @@
+function DatosPersonas()
+{
+
+	var n=prompt("Ingrese Nombre");
+	var a=prompt("ingrese Apellidos");
+	var e=prompt("ingrese Edad");
+	var p=prompt("ingrese Profesion");
+
+	
+	var Persona
+	{
+		Nombre="",
+		Apellidos="",
+		Edad=0,
+		Profesion
+
+
+	}
+}
