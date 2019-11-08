a87d512af679d3990ebdae1cd4815a055fec1e9d
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-08 15:08:46 -0300

    declaracion objeto persona

---
+++ Ejercicio8/index.js
@@ -0,0 +1,13 @@
+// Escribir un algoritmo solicite la siguiente información de una persona:
+// Nombre: debe aceptar solo texto
+// Apellidos: debe aceptar solo texto
+// Edad: debe aceptar solo número
+// Profesión: debe aceptar solo texto
+// Mostrar información por pantalla (a elección)
+// Utilizar OBJETOS
+
+var Persona = new Object();
+Persona.nombre='Jorge';
+Persona.apellidos='Gajardo Reyes';
+Persona.edad='26';
+Persona.profesion='Ing. Informatico';
