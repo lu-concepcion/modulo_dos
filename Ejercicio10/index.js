ffe5cca33ae210f9c0f6abedf3f085683b410e0c
melinag <melinag@lagash.com>
2019-11-08 18:08:21 -0300

    se añade ejercicio 10 terminado

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -2,3 +2,46 @@
 //input: X
 //output: Diez
 //Utilizar OBJETOS
+var numero = {}
+var numero = [
+    { romano: "I",
+      español: "uno"
+     },
+     { romano: "II",
+         español:"dos"
+        },
+        { romano: "III",
+         español:"tres"
+        },
+        { romano: "IV",
+         español:"cuatro"
+        },
+        { romano: "V",
+         español: "cinco"
+        },
+        { romano: "VI",
+         español:"seis"
+        },
+        { romano: "VII",
+         español:"siete"
+        },
+        { romano: "VIII",
+         español:"ocho"
+        },
+        { romano:"IX", 
+         español:"nueve"
+        },
+        { romano:"X",
+         español:"diez"
+        }
+     ]          
+
+    var respuesta= prompt("ingrese numero romano en mayúsculas: ")
+
+ for (let i = 0; i < numero.length; i++) {
+     if (numero[i].romano == respuesta )
+     {  document.write(numero[i].español) }
+     
+ }
+
+
