a98048a7f5fb5f1867aadf63809f4eb06095b1c3
marcoac001 <marcoac001@lagash.com>
2019-11-08 17:03:35 -0300

    Se agrega un switch para cada caso

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -1,44 +1,62 @@
-edad = 0;
-nombre = "";
-apellido = "";
-profesion = "";
+var numeros_romanos = {
+    I = "Uno",
+    II = "Dos",
+    III = "Tres",
+    IV = "Cuatro",
+    V = "Cinco",
+    VI = "Seis",
+    VII = "Siete",
+    VIII = "Ocho",
+    IX = "Nueve",
+    X = "Diez"
+};
+flag = 0;
 
-var persona = new Object();
-
-do {
-    var edad = prompt("Ingrese una edad");
-} while (Number.isNaN(parseInt(edad)));
-persona.edad = edad;
-
-do {
-    var nombre = prompt("Ingrese sus nombres");
-} while (Number.isNaN(parseInt(nombre)) == false);
-persona.nombre = nombre;
-
-do {
-    var apellido = prompt("Ingrese sus apellidos");
-} while (Number.isNaN(parseInt(apellido)) == false);
-persona.apellido = apellido
-do {
-    var profesion = prompt("Ingrese su profesion");
-} while (Number.isNaN(parseInt(profesion)) == false);
-persona.profesion = profesion
-
-var consultar_opcion = prompt("¿Que deseas ver?" + "\n1.- Nombre" + "\n2.- Apellido " + "\n3.- Edad" + "\n4.- Profesion" + "\n5.-Todo");
-if (consultar_opcion == 1) {
-    alert(persona.nombre);
-} else {
-    if (consultar_opcion == 2) {
-        alert(persona.apellido);
-    } else {
-        if (consultar_opcion == 3) {
-            alert(persona.edad)
-        } else {
-            if (consultar_opcion == 4) {
-                alert(persona.profesion)
-            } else {
-                alert("Nombre: " + persona.nombre + "\nApellido: " + persona.apellido + "\nEdad: " + persona.edad + "\nProfesion: " + persona.profesion)
-            }
-        }
-    }
+do{
+var consultar_opcion = prompt ("Ingrese su numero romano");
+consultar_opcion = consultar_opcion.toLocaleUpperCase;
+switch(consultar_opcion){
+    case I:
+        alert(numeros_romanos.I)
+        flag=1;
+        break;
+    case II:
+        alert(numeros_romanos.II)
+        flag=1;
+        break;
+    case III:
+        alert(numeros_romanos.III)
+        flag=1;
+        break;
+    case IV:
+        alert(numeros_romanos.IV)
+        flag=1;
+        break;
+    case V:
+        alert(numeros_romanos.V)
+        flag=1;
+        break;
+    case VI:
+        alert(numeros_romanos.VI)
+        flag=1;
+        break;
+    case VII:
+        alert(numeros_romanos.VII)
+        flag=1;
+        break;
+    case VIII:
+        alert(numeros_romanos.VIII)
+        flag=1
+        break;
+    case IX:
+        alert(numeros_romanos.IX)
+        flag=1;
+        break;
+    case X:
+        alert(numeros_romanos.X)
+        flag=1
+        break;
+    default:
+        alert("El valor ingresado no es romano")
 }
+}while(flag==0);
