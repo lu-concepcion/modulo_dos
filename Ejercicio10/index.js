c227fc82469b459be5938deb11e3099dae70fae6
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-08 18:08:40 -0300

    crear array con numeros romanos y su nombre

---
+++ Ejercicio10/index.js
@@ -0,0 +1,42 @@
+var numeroRomano = [
+    {
+        romano: "I",
+        nombre: "uno"
+    },
+    {
+        romano: "II",
+        nombre: "dos"
+    },
+    {
+        romano: "III",
+        nombre: "tres"
+    },
+    {
+        romano: "IV",
+        nombre: "cuatro"
+    },
+    {
+        romano: "V",
+        nombre: "cinco"
+    },
+    {
+        romano: "VI",
+        nombre: "seis"
+    },
+    {
+        romano: "VII",
+        nombre: "siete"
+    },
+    {
+        romano: "VIII",
+        nombre: "ocho"
+    },
+    {
+        romano: "IX",
+        nombre: "nueve"
+    },
+    {
+        romano: "X",
+        nombre: "diez"
+    },
+];
