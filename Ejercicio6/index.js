1579832ac3c5bf20219f820fac54bf33e14853f6
jeremya-lagash <jeremya@lagash.com>
2019-11-07 22:53:32 -0300

    Se ha arreglado el ejercicio6

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,14 +1,29 @@
 var numerosPares = [];
 var numerosIngresados = [];
-var suma, multiplic = 0;
+var suma = 0;
+var multiplic = 1;
+
 for(var i=0; i<5; i++){
     numerosIngresados[i] = prompt("Ingrese un número par");
-    if(numerosIngresados[i] % 2! = 0 || numerosIngresados [i]<0 || numerosIngresados[i]>20){
-        numerosIngresados [i] = prompt("Número inválido, ingrese nuevamente");
+    if(numerosIngresados[i] % 2 != 0 || numerosIngresados [i]<0 || numerosIngresados[i]>20){
+        numerosIngresados [i] = prompt("Por favor ingrese un número par");
       if (numerosIngresados [i] % 2== 0 && numerosIngresados [i] >0 && numerosIngresados [i]<21)
       {
-         numerosPares[i] = numerosIngresados[i];
+         numerosPares[i]=numerosIngresados[i];
       }
     }
+    numerosPares[i]=numerosIngresados[i];
 }
-alert("")
+
+var operacion = prompt("Ingrese que operación desea realizar (Sumar - Multiplicar)");
+  if (operacion == "Sumar"){ 
+    for (var j = 0; j<5; j++){
+      suma = parseInt(suma)+ parseInt(numerosPares[j]);
+    }alert(suma)
+  } 
+  
+  if (operacion == "Multiplicar"){
+    for (var j = 0; j<5; j++){
+      multiplic = parseInt(multiplic) * parseInt(numerosPares[j]);
+    }alert(multiplic)
+ }
