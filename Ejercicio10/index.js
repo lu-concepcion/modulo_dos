08b8aea31de35b4ac2c27c86a33652777e3f1e2b
nicolasar-lagash <nicolasar@lagash.com>
2019-11-08 17:18:12 -0300

    Ejercicio 10 terminado

---
+++ Ejercicio10/index.js
@@ -0,0 +1,57 @@
+var listNumeros = [
+    {
+        numero: "i",
+        valor: "uno"
+    },
+    {
+        numero: "ii",
+        valor: "dos"
+    },
+    {
+        numero: "iii",
+        valor: "tres"
+    },
+    {
+        numero: "iv",
+        valor: "cuatro"
+    },
+    {
+        numero: "v",
+        valor: "cinco"
+    },
+    {
+        numero: "vi",
+        valor: "seis"
+    },
+    {
+        numero: "vii",
+        valor: "siete"
+    },
+    {
+        numero: "viii",
+        valor: "ocho"
+    },
+    {
+        numero: "ix",
+        valor: "nueve"
+    },
+    {
+        numero: "x",
+        valor: "diez"
+    },
+];
+
+var input = "";
+do{
+    input = prompt("Ingrese su numero romano");
+}while(input.length <= 0 || input.length >=5)
+
+for(var i=0; i<10; i++){
+    if(listNumeros[i].numero == input.toLowerCase()){
+        alert("El valor es " + listNumeros[i].valor);
+        break;
+    }
+    if(i == 9){
+        alert("Numero desconocido");
+    }
+}
