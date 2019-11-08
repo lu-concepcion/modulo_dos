ccff6cef0883697ac25a8f24d6491f845f8c1310
marcoac001 <marcoac001@lagash.com>
2019-11-08 18:17:41 -0300

    Agregué las variables que me servirán

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -1,53 +1,10 @@
-var datos_persona = {
-    edades : [],
-    nombres : [],
+contador = 0;
+var persona = {
+    nombre: "",
+    edad: 0,
     mayor: {
-        nombre: "",
-        edad: 0
+        edad: "",
+        nombre: 0
     }
 };
 
-flag=1;
-bucle_infinito=1;
-contador=0;
-auxiliar=0;
-
-do{
-    for(i=0;i<bucle_infinito;i++){
-        do{
-            var solicitar_edad = prompt("Ingrese la edad de la persona");
-            if(solicitar_edad=="Salir"||solicitar_edad=="salir"){
-                if(contador>=4){
-                    auxiliar=i;
-                    bucle_infinito=i;
-                    flag=0;
-                    break;
-                }
-            }
-        }while(Number.isNaN(parseInt(solicitar_edad)));
-        datos_persona.edades[i] = solicitar_edad;
-
-        do{
-            var solicitar_nombre = prompt("Ingrese el nombre de esa persona");
-            if(solicitar_nombre=="Salir"||solicitar_nombre=="salir"){
-                if(contador>=4){
-                    auxiliar=i;
-                    bucle_infinito=i;
-                    flag=0
-                    break;
-                }
-            }
-        }while(Number.isNaN(parseInt(solicitar_nombre))==false);
-        datos_persona.nombres[i] = solicitar_nombre;
-        contador++;
-        bucle_infinito++;
-    }
-}while(flag==1);
-
-for(i=0;i<=auxiliar;i++){
-    if(datos_persona.edades[i]>datos_persona.mayor.edad){
-        datos_persona.mayor.edad = datos_persona.edades[i];
-        datos_persona.mayor.nombre = datos_persona.nombres[i];
-    }
-}
-alert(datos_persona.mayor.nombre+" es el(la) mayor con "+datos_persona.mayor.edad+" años.")
