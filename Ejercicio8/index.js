a61af5e094b0dc50e6f0b06bce7fbe6e687d9ea2
bryanm-lagash <bryanm@lagash.com>
2019-11-08 15:56:13 -0300

    Ejercicio casi completo

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -16,4 +16,23 @@ do{
     persona.profesion = prompt("Ingrese profesion: ");
 }while(isNaN(persona.profesion) == false)
 
-alert(persona.nombre + " " + persona.apellidos + " " + persona.edad + " " + persona.profesion);
+var respuesta = prompt("¿Que información desea ver?" +
+                        "1.- Nombre" +
+                        "2.- Apellidos" +
+                        "3.- Edad" +
+                        "4.- Profesión" );
+
+switch(respuesta){
+    case 1:
+        alert(persona.nombre);
+        break;
+    case 2:
+        alert(persona.apellidos);
+        break;
+    case 3:
+        alert(persona.edad);
+        break;
+    case 4:
+        alert(persona.profesion);
+        break;
+}
