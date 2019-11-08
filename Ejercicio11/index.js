231275d014043c283d2e6999a82107a7d4208ad7
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-11-08 18:35:59 -0300

    script no culminado ):

---
+++ Ejercicio11/index.js
@@ -0,0 +1,10 @@
+var contador = 0;
+let nombres = new Object();
+let edades = new Object();
+
+do {
+    var nombre = prompt("Introduce tu nombre:");
+    if ((nombre !== "" && isNaN(nombre)) && (nombre.toLowerCase !== "salir")) {
+        nombres.
+    }
+}while(!paso);
