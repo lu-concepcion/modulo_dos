34c6415c51e95e32b1f6318a385000f35fa53d09
felipep-lagash <felipep@lagash.com>
2019-11-08 15:34:14 -0300

    Se optimizó el código de las validaciones de usuario

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -3,41 +3,34 @@ let apellido = ""
 let edad = ""
 let profesion = ""
 
-
 while(true){
     nombre = prompt('Ingresa nombre');
-    if (!nombre.match(/[a-zA-Z]/) || nombre === '') {
-        alert('Error: Ingrese solo letras y no dejes campo vacío');
-    } else {
+    if (nombre.match(/[a-zA-Z]/) && nombre != '') {
         break;
     }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
 }
 while(true){
     apellido = prompt('Ingresa apellido');
-    if (!nombre.match(/[a-zA-Z]/) || apellido === '') {
-        alert('Error: Ingrese solo letras y no dejes campo vacío');
-    } else {
+    if (nombre.match(/[a-zA-Z]/) && apellido != '') {
         break;
     }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
 }
 while(true){
     edad = prompt('Ingresa edad');
-    if (!/^([0-9])*$/.test(edad) || edad === '') {
-        alert('Error: Ingrese solo números y no dejes campo vacío');
-    } else {
+    if (/^([0-9])*$/.test(edad) && edad != '') {
         break;
     }
+    alert('Error: Ingrese solo números y no dejes campo vacío');
 }
 while(true){
     profesion = prompt('Ingresa profesion');
-    if (!nombre.match(/[a-zA-Z]/) || profesion === '') {
-        alert('Error: Ingrese solo letars y no dejes campo vacío');
-    } else {
+    if (nombre.match(/[a-zA-Z]/) && profesion != '') {
         break;
     }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
 }
-
-
 
 var persona = new Object();
 
