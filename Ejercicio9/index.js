7d0ff8e4b19739364b19dd45106ac4d91fa14ab2
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 16:17:49 -0300

    ingreso de las variables

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -3,3 +3,15 @@ do
 {
     auto.marca=prompt("ingrese marca")
 } while(parseInt(auto.marca))
+do
+{
+    auto.modelo=prompt("ingrese modelo")
+} while(parseInt(auto.marca))
+do
+{
+    auto.año=prompt("ingrese año")
+} while(parseInt(auto.marca))
+do
+{
+    auto.color=prompt("ingrese color")
+} while(parseInt(auto.marca))
