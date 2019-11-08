be4ef27cc8616fa2d7e0947b7dc1da6e30e9bcff
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 17:02:42 -0300

    ejercicio 9 terminado

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -1,5 +1,5 @@
 var auto = Object()
-var flag = 1
+var flag = false
 do
 {
     auto.marca=prompt("ingrese marca")
@@ -12,37 +12,35 @@ do
 {
     auto.año=prompt("ingrese año")
 } while(isNaN(auto.año))
+auto.color= Object()
 do
 {
-    auto.color=prompt("ingrese color")
-} while(parseInt(auto.color))
+    auto.color.nombre=prompt("ingrese color")
+} while(parseInt(auto.color.nombre))
 do
 {
-    auto.colorHEX=prompt("ingrese color (HEX)")
-} while(isNaN(auto.colorHEX))
+    auto.color.HEX=prompt("ingrese color (HEX)")
+} while(parseInt(auto.color.HEX))
 do{
     info= prompt("¿que informacion desea ver?")
     switch(info)
     {
         case "marca":
         alert(auto.marca)
-        flag = 0;
         break
         case "modelo":
         alert(auto.modelo)
-        flag = 0;
         break
         case "año":
         alert (auto.año) 
-        flag = 0;
         break 
         case "color":
         alert (auto.color.nombre)
-        flag = 0;
+        case "color(HEX)":
+        alert (auto.color.HEX)
         break
         default:
         alert ("expresion invalida, intente nuevamente")
-    
-    
+        flag = true
     }
     }while(flag)
