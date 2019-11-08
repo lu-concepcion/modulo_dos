2c4f0f8e857dc680a11b4b862d9f79b97f611578
marcoac001 <marcoac001@lagash.com>
2019-11-08 15:37:52 -0300

    Le agregué espacios al documento

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -5,40 +5,40 @@ profesion = "";
 
 var persona = new Object();
 
-do{
-    var edad = prompt ("Ingrese una edad");
-}while(Number.isNaN(parseInt(edad)));
+do {
+    var edad = prompt("Ingrese una edad");
+} while (Number.isNaN(parseInt(edad)));
 persona.edad = edad;
 
-do{
+do {
     var nombre = prompt("Ingrese sus nombres");
-}while(Number.isNaN(parseInt(nombre))==false);
+} while (Number.isNaN(parseInt(nombre)) == false);
 persona.nombre = nombre;
 
-do{
+do {
     var apellido = prompt("Ingrese sus apellidos");
-}while(Number.isNaN(parseInt(apellido))==false);
+} while (Number.isNaN(parseInt(apellido)) == false);
 persona.apellido = apellido
-do{
+do {
     var profesion = prompt("Ingrese su profesion");
-}while(Number.isNaN(parseInt(profesion))==false);
+} while (Number.isNaN(parseInt(profesion)) == false);
 persona.profesion = profesion
 
-var consultar_opcion = prompt ("¿Que deseas ver?"+ "\n1.- Nombre" + "\n2.- Apellido "+ "\n3.- Edad"+ "\n4.- Profesion"+"\n5.-Todo");
-if(consultar_opcion==1){
+var consultar_opcion = prompt("¿Que deseas ver?" + "\n1.- Nombre" + "\n2.- Apellido " + "\n3.- Edad" + "\n4.- Profesion" + "\n5.-Todo");
+if (consultar_opcion == 1) {
     alert(persona.nombre);
-}else{
-    if(consultar_opcion==2){
+} else {
+    if (consultar_opcion == 2) {
         alert(persona.apellido);
-    }else{
-        if(consultar_opcion==3){
+    } else {
+        if (consultar_opcion == 3) {
             alert(persona.edad)
-        }else{
-            if(consultar_opcion==4){
-            alert(persona.profesion)
-            }else{
-                alert("Nombre: "+persona.nombre+"\nApellido: "+persona.apellido+"\nEdad: "+persona.edad+"\nProfesion: "+persona.profesion)
-            }
+        } else {
+            if (consultar_opcion == 4) {
+                alert(persona.profesion)
+            } else {
+                alert("Nombre: " + persona.nombre + "\nApellido: " + persona.apellido + "\nEdad: " + persona.edad + "\nProfesion: " + persona.profesion)
             }
         }
     }
+}
