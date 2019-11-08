f011f160deccf313f8f76a52ef27b59e9fb3fa23
jeremya-lagash <jeremya@lagash.com>
2019-11-08 14:59:55 -0300

    Se ha agregado el ejercicio8 vacio

---
+++ Ejercicio8/index.js
@@ -0,0 +1,8 @@
+//Escribir un algoritmo solicite la siguiente información de una persona:
+//Nombre: debe aceptar solo texto
+//Apellidos: debe aceptar solo texto
+//Edad: debe aceptar solo número
+//Profesión: debe aceptar solo texto
+//Mostrar información por pantalla (a elección)
+//Utilizar OBJETO
+
