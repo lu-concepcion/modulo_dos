b476125f31b7586f32aa83b4a2d6a7d082356a3f
estebanl-lagash <estebanl@lagash.com>
2019-11-08 18:18:38 -0300

    actualizacion ejercicio 10 - segunda parte - nada
    solucionar prrblematica de compatibilidad arrays

---
+++ Ejercicio10/index.js
@@ -0,0 +1,71 @@
+
+var romano = [
+{
+
+    numero = "1",
+    valor = "I"
+
+},{
+
+    numero = "2",
+    valor = "II"
+
+},{
+
+    numero = "3",
+    valor = "III"
+
+},{
+
+    numero = "4",
+    valor = "IV"
+
+},{
+
+    numero = "5",
+    valor = "V"
+
+},{
+
+    numero = "6",
+    valor = "VI"
+
+},{
+
+    numero = "7",
+    valor = "VII"
+
+},{
+  
+    numero = "8",
+    valor = "VIII"
+
+},{
+
+    numero = "9",
+    valor = "IX"
+
+},{
+
+    numero = "10",
+    valor = "X"
+
+}];
+
+
+var numIngresado = prompt ("Ingrese un numero romano a convertir");
+
+
+
+var romano={
+
+    numConvertir: numIngresado,
+    numRomano: numero
+
+};
+
+var { numConvertir, numRomano } = romano;
+alert(`${numConvertir} ${numRomano}`); 
+
+
+    
