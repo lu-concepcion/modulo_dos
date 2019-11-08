6d4db4722b876ac584dd32dafab43e54c2edb537
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 15:13:26 -0300

    agregué objeto y entrada de info

---
+++ Ejercicio8/index.js
@@ -0,0 +1,10 @@
+var persona = {
+    nombre:""
+    apellido:""
+    edad:0
+    profesion:""
+}
+persona.nombre= prompt("ingrese nombre")
+persona.apellido= prompt("ingrese apellido")
+persona.edad= prompt("ingrese edad")
+persona.profesion= prompt("ingrese profesion")
