e3a331e20005224e43de57ce9e36f7360fc1ca4a
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 18:39:43 -0300

    entrada de datos, ejercicio 11

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -1,6 +1,14 @@
-var persona = Object()
+var persona = [
+    {
+        nombre:"",
+        edad:"0",
+    }]
 var edadEntrada = 0
 var cont = 0
 do{
-    persona[cont].nombre = prompt("ingrese un nombre"))
+    var nombre = prompt("ingrese un nombre")
+    var edad = prompt("ingrese una edad")
+    persona.push({ nombre: nombre, edad: edad })
+    cont++
+    console.log(persona)
 }while(cont<5||entrada!="salir")
