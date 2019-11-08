ff50b132e1ada4c9804fc331d64bfb91868be662
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-08 16:45:17 -0300

    agrega menu de opciones para mostrar en pantalla

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -20,10 +20,46 @@ while(isNaN(auto.año))
 {
     auto.marca = prompt("Error, ingrese nuevamente");
 }
+auto.color = new Object();
+auto.color.codigo = prompt("Ingrese el codigo del color de su automóvil");
 
-auto.color = prompt("Ingrese el color de su automóvil");
-
-while(!isNaN(auto.color))
+while(!isNaN(auto.color.codigo))
 {
     auto.color = prompt("Error, ingrese nuevamente");
+}
+
+auto.color.paleta = prompt("Ingrese el nombre del color de su automóvil");
+
+while(!isNaN(auto.color.paleta))
+{
+    auto.color = prompt("Error, ingrese nuevamente");
+}
+var colorTodo = auto.color.codigo + "-" + auto.color.paleta;
+var opcion = prompt("Ingrese una de las siguientes opciones para mostrar en pantalla:\n 0:marca\n 1:modelo\n 2:año\n 3:código del color\n 4:nombre del color\n 5:código y nombre del color");
+
+switch (opcion)
+{
+    case "0":
+        alert(auto.marca)
+        break;
+    
+    case "1":
+        alert(auto.modelo)
+        break;
+        
+    case "2":
+        alert(auto.año)
+        break;
+        
+    case "3":
+        alert(auto.color.codigo)
+        break;
+        
+    case "4":
+        alert(auto.color.paleta)
+        break;
+                
+    case "5":
+        alert(colorTodo)
+        break;
 }
