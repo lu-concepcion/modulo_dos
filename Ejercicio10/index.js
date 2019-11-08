fcee33e482632c59692d32997a3d9720089bc1c4
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-11-08 17:50:29 -0300

    Se completo el Ejercicio 10 (con ayuda de Hernán)

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -1,4 +1,59 @@
 function Numeros()
 {
-	var numero = prompt("ingresa un numero Romano");
+	var input =  prompt("ingresa un numero Romano").toUpperCase() ;
+
+		var numerosRomanos =[
+			{
+				numero: "uno",
+				numeroRomano: "I"
+			},
+			{
+				numero: "dos",
+				numeroRomano: "II"
+			},
+			{
+				numero: "tres",
+				numeroRomano: "III"
+			},
+			{
+				numero: "cuatro",
+				numeroRomano: "IV"
+			},
+			{
+				numero: "cinco",
+				numeroRomano: "V"
+			},
+			{
+				numero: "seis",
+				numeroRomano: "VI"
+			},
+			{
+				numero: "siete",
+				numeroRomano: "VII"
+			},
+			{
+				numero: "ocho",
+				numeroRomano: "VIII"
+			},{
+				numero: "nueve",
+				numeroRomano: "IV"
+			},{
+				numero: "diez",
+				numeroRomano: "X"
+			},
+		];
+
+	
+	for(var i=0;i<10;i++)
+	{
+		if(numerosRomanos[i].numeroRomano==input)
+		{
+			alert(numerosRomanos[i].numero);
+			break;
+
+		}
+	}
+
+
+	
 }
