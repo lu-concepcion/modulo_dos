09e76305dcda7e6de307479f17aad05633856cb6
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-11-08 16:09:48 -0300

    Actualizacion del script

---
+++ Ejercicio8/index.js
@@ -0,0 +1,56 @@
+// Defino las variables
+var persona = new Object();
+
+do {
+    var nombre = prompt("Cual es tu Nombre?");
+    if (nombre !== "" || isNaN(nombre)) {
+        persona.Nombre = nombre;
+         var pasoNombre = true;
+    }
+    else {
+        alert("Ingresa tu nombre correctamente!");
+        var pasoNombre = false;
+    }
+}while(!pasoNombre);
+
+do {
+    var apellido = prompt("Cuales son tus apellidos?");
+    if (apellido !== "" || isNaN(apellido)) {
+        persona.Apellido = apellido;
+         var pasoApellido = true;
+    }
+    else {
+        alert ("Ingresa tu apellido correctamente!");
+        var pasoApellido = false;
+    }
+}while(!pasoApellido);
+
+do {
+    var profession = prompt("Cual es tu profession?");
+    if (profession !== "" || isNaN(profession) ) {
+        persona.Profession = profession;
+        var pasoProfression = true;
+    }
+    else {
+        alert ("Ingresa tu profession correctamente!");
+        var pasoProfession = false;
+    }
+}while(!pasoProfession);
+
+do {
+    var edad = prompt("Cual es tu edad?");
+    if (edad !== "" || !isNaN(profession) || edad !== 0) {
+        persona.Edad = edad;
+        var pasoEdad = true;
+    }
+    else {
+        alert ("Ingresa tu profession correctamente!");
+        var pasoEdad = false;
+    }
+}while(!pasoEdad);
+
+var datos = prompt("Si deseas ver tu datos ingresa SI");
+
+if (datos.toLowerCase() == "si") {
+    alert("Tu nombre es: " +persona[Nombre] + "\nTu apellido es: " +persona[Apellido]);
+}
