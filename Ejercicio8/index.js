6e6b225e751f11c5ee14bef6d8b32f880041781b
nicolasar-lagash <nicolasar@lagash.com>
2019-11-08 15:46:48 -0300

    Ejercicio 8 estructura base
    Estructura del objeto y logica del primer do while

---
+++ Ejercicio8/index.js
@@ -0,0 +1,12 @@
+var persona = {
+    nombre: "",
+    apellidos: "",
+    edad: 0,
+    profesion: ""
+};
+
+do{ //Nombre
+    persona.nombre = prompt("Ingrese su nombre");
+}while(parseInt(persona.nombre));
+
+
