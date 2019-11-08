2307c910d426031e85dd1db852d4f18290607596
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-08 17:15:00 -0300

    Se incluye el ingreso de colores su validacion.  Se incluye la opcion de mostrar la informacion ingresada

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -27,3 +27,39 @@ informacionVehiculo.Año = prompt("Ingrese el año del vehículo");
 while( isNaN(informacionVehiculo.Año ) == true){
     informacionVehiculo.Año  = prompt("Ingrese el año del vehículo");
 }
+
+
+
+var colores ={};
+colores.HTML = prompt("Ingrese el color del vehículo en formato HTML");
+while((colores.HTML !== "FF0000") && (colores.HTML !== "008000") && (colores.HTML !== "0000FF")){
+    colores.HTML  = prompt("Ingrese el color del vehículo en formato HTML");
+}
+
+
+
+
+colores.Nombre  = prompt("Ingrese el nombre del color del vehículo");
+if(colores.HTML =="FF0000"){
+    while(colores.Nombre !== "Rojo"){
+        colores.Nombre  = prompt("Ingrese el nombre del color del vehículo");
+    }
+} else if(colores.HTML =="008000"){
+    while(colores.Nombre !== "Verde"){
+        colores.Nombre  = prompt("Ingrese el nombre del color del vehículo");
+    }
+} else if(colores.HTML =="0000FF"){
+    while(colores.Nombre !== "Azul"){
+        colores.Nombre  = prompt("Ingrese el nombre del color del vehículo");
+    }
+}
+
+informacionVehiculo.Color= colores;
+
+var verInformacion ="";
+
+verInformacion= prompt("Desea ver la informacion en pantalla? Ingrese Si o No");
+
+if ( verInformacion == "Si"){
+    alert("Modelo: " + informacionVehiculo.Modelo + " Marca: " + informacionVehiculo.Marca + " Año: " + informacionVehiculo.Año + " Color HTML : " + informacionVehiculo.Color.HTML + " Nombre Color : " + informacionVehiculo.Color.Nombre);
+}
