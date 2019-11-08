8bc082f2376eba232da7a0f10acbfe3c48fd4532
felipep-lagash <felipep@lagash.com>
2019-11-08 16:04:39 -0300

    Se agregaron validaciones a entradas de información

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -1,7 +1,36 @@
-let marca = ""
-let modelo = ""
-let anio = ""
-let color = ""
+let marca = "";
+let modelo = "";
+let anio = "";
+let color = "";
+
+while(true){
+    marca = prompt('Ingresa marca');
+    if (isNaN(marca) && marca != '') {
+        break;
+    }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
+}
+while(true){
+    modelo = prompt('Ingresa modelo');
+    if (isNaN(modelo) && modelo != '') {
+        break;
+    }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
+}
+while(true){
+    anio = prompt('Ingresa año');
+    if (!isNaN(anio) && anio != '') {
+        break;
+    }
+    alert('Error: Ingrese solo números y no dejes campo vacío');
+}
+while(true){
+    color = prompt('Ingresa color');
+    if (isNaN(color) && color != '') {
+        break;
+    }
+    alert('Error: Ingrese solo letras y no dejes campo vacío');
+}
 
 var vahiculo = new Object();
 
@@ -14,4 +43,5 @@ document.write(vahiculo.marca + ', ');
 document.write(vahiculo.modelo + ', ');
 document.write(vahiculo.anio + ', ');
 document.write(vahiculo.color);
+console.log(color);
 
