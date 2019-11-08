06ed9da72bff7e5f23e1fd04666e19b62ca550eb
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-08 18:31:40 -0300

    Ejercicio10 por terminar me falta la validacion que no este fuera del rango

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -1,5 +1,58 @@
-// variable edad guardará lo introducido en el prompt
-var edad = prompt("Introduce tu mensaje");
+/*
+Escribir un algoritmo que indique, dado un número romano, este mismo en palabras,
+ solo del 1 al 10, ej;
 
-// levanta una ventana con la edad ingresada
-alert(edad)
+input: X
+output: Diez
+
+Utilizar OBJETOS*/
+
+var nn = prompt("Ingresa un numero Romano: ").toUpperCase();
+var numeror = [
+{
+    numero: "uno",
+    numeroromano: "I"
+},
+{
+    numero: "Dos",
+    numeroromano: "II"
+},
+{
+    numero: "Tres",
+    numeroromano: "IIi"
+},
+{
+    numero: "Cuatro",
+    numeroromano: "IV"
+},
+{
+    numero: "Cinco",
+    numeroromano: "V"
+},
+{
+    numero: "Seis",
+    numeroromano: "VI"
+},
+{
+    numero: "Siete",
+    numeroromano: "VII"
+},    
+{
+    numero: "Ocho",
+    numeroromano: "VIII"
+},
+{
+    numero: "Nueve",
+    numeroromano: "IX"
+},
+{
+    numero: "Dies",
+    numeroromano: "X"
+},
+]
+for(var i = 0; i<10;i++)
+if(nn==numeror[i].numeroromano){
+    document.write("El numero romanos es: "+ numeror[i].numero);
+    break;
+
+}
