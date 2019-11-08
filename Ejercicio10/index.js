14115d2dedc20aed9e240a5a550ea312c90cdc6d
felipep-lagash <felipep@lagash.com>
2019-11-08 17:24:05 -0300

    Se agregó validación para ingresar números romanos

--- Ejercicio10/index.js
+++ Ejercicio10/index.js
@@ -3,42 +3,48 @@ romano = {
     numNormal : 0
 }
 
-romano.numRomano = prompt('Ingresa número romando (solo del 1 al 10').toUpperCase();
 
-switch (romano.numRomano) {
-    case 'I':
-        romano.numNormal = 1;
-        break;
-    case 'II':
-        romano.numNormal = 2;
-        break;
-    case 'III':
-        romano.numNormal = 3;
-        break;
-    case 'IV':
-        romano.numNormal = 4;
-        break;
-    case 'V':
-        romano.numNormal = 5;
-        break;
-    case 'VI':
-        romano.numNormal = 6;
-        break;
-    case 'VII':
-        romano.numNormal = 7;
-        break;
-    case 'VIII':
-        romano.numNormal = 8;
-        break;
-    case 'IX':
-        romano.numNormal = 9;
-        break;
-    case 'X':
-        romano.numNormal = 10;
-        break;
-    default:
-        break;
-}
+do {
+    romano.numRomano = prompt('Ingresa número romando (solo del 1 al 10').toUpperCase();
+    switch (romano.numRomano) {
+        case 'I':
+            romano.numNormal = 1;
+            break;
+        case 'II':
+            romano.numNormal = 2;
+            break;
+        case 'III':
+            romano.numNormal = 3;
+            break;
+        case 'IV':
+            romano.numNormal = 4;
+            break;
+        case 'V':
+            romano.numNormal = 5;
+            break;
+        case 'VI':
+            romano.numNormal = 6;
+            break;
+        case 'VII':
+            romano.numNormal = 7;
+            break;
+        case 'VIII':
+            romano.numNormal = 8;
+            break;
+        case 'IX':
+            romano.numNormal = 9;
+            break;
+        case 'X':
+            romano.numNormal = 10;
+            break;
+        default:
+            continue;
+    }
+    break;
+} while(true);
+
+
+
 
 alert(romano.numRomano + ' es el número: ' + romano.numNormal)
 
