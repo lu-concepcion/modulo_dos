808b846a39682f460b4a1fe58656a7b2d8986eec
fabianb-lagash <fabianb@lagash.com>
2019-11-08 16:42:18 -0300

    Se agregaron cambios al ejercicio 8

--- ejercicio8/index.js
+++ ejercicio8/index.js
@@ -3,21 +3,34 @@ var persona = new Object ();
 var name;
 var age; 
 var apellido;
+var job;
 
-nombre = prompt("Ingrese nombre");
-if (name != parseInt(name)) {
-    persona.nombre = name
-} else prompt ("Vuelva a ingresar su nombre");
-alert(name)
+
+name = prompt("Ingrese nombre");
+
+do {
+    if (name != parseInt(name)) {
+        persona.nombre = name
+    } else prompt ("Vuelva a ingresar su nombre");
+    
+} while (name == parseInt(name));
+
+
 
 apellido = prompt("ingrese sus apellidos");
 if (apellido != parseInt(apellido)) {
     persona.apellidos = apellido
 } else prompt ("vuelva a ingresar sus apellidos");
-alert(apellido)
+
 
 age = prompt ( "ingrese su edad"); 
-if (age = parseInt(age)) {
+if (age != isNaN(age)) {
     persona.edad = age
 } else prompt ("Vuelva a ingresar su edad");
-alert(age);
+
+job = prompt("Ingrese su profesión")
+if (job != parseInt(job)) {
+    persona.profesion = job
+} else prompt ("vuelva a ingresar su profesión")
+
+alert(name +"-" + apellido + "-" + age + "-" + job )
