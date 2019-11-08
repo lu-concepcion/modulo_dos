7a64f90061b36095352a10f50b0f12afe2493a2f
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-08 15:30:24 -0300

    agrega validación de edad,solicitud de apellido y concatena nombre y apellido

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,4 +1,15 @@
 var persona = new Object ();
 
 persona.nombre = prompt("Ingrese su nombre");
-alert(persona.nombre)
+persona.apellido = prompt("Ingrese su apellido");
+persona.nombreCompleto = `${persona.nombre} ${persona.apellido}`;
+``
+//alert(persona.nombre)
+//65-90
+//97-122
+persona.edad = prompt("Ingrese su edad");
+if (isNaN(persona.edad))
+{
+    persona.edad = prompt("Error, Ingrese su edad");
+}
+alert(persona.edad)
