bf8e3dd1f3a3f085acee0acd90368bae56e96c3a
melinag <melinag@lagash.com>
2019-11-08 12:26:01 -0300

    ejercicio 6 terminado

--- Ejercicio6/index.js
+++ Ejercicio6/index.js
@@ -1,5 +1,5 @@
-var contador = 0;
 var numerosPares = [];
+var resultado ="";
 
 do {
     var p = prompt("ingrese un numero par");
@@ -9,6 +9,13 @@ do {
     }
     }while(numerosPares.length < 5);
 
-do {
-    
-}
+do { var operacion = prompt("desea realizar multiplicación o adición de los valores?" )
+        if (operacion == "multiplicación" || operacion == "multiplicacion" ){
+             resultado = parseInt(numerosPares[0])* parseInt(numerosPares[1])* parseInt(numerosPares[2]) * parseInt(numerosPares[3]) * parseInt(numerosPares[4]);
+             document.write(resultado);
+         }
+        if (operacion =="adición" || operacion == "adicion"){
+            resultado = parseInt(numerosPares[0])+ parseInt(numerosPares[1])+ parseInt(numerosPares[2])+ parseInt(numerosPares[3]) + parseInt(numerosPares[4]);
+            document.write(resultado);
+         }       
+}while(resultado == "" );
