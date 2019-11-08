3e2a315f44a5c149454b32680126ad3305b12f3d
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-08 12:29:35 -0300

    Agregar Ejercicio7 Terminado definitivamente

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,7 +1,20 @@
 
 var f=[0,1];
+var error="false";
 
-var numero = parseInt(prompt("Ingrese numero: "));
+do
+{
+    var numero = parseInt(prompt("ingresa un numero"));
+     error="false";
+
+    if(numero<3 || numero>15)
+    {
+       alert("Fuera de rango!");
+       error="true";
+
+    }
+
+}while(error=="true");
 
 
 for(var i=2;i<numero;i++){
