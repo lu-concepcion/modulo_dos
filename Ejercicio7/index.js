2d7ea979a8135a84d391f7302da1b4b23760d945
felipep-lagash <felipep@lagash.com>
2019-11-07 23:17:23 -0300

    Se mejoró algoritmo de generación de figonacci dentro de ciclo for

--- Ejercicio7/index.js
+++ Ejercicio7/index.js
@@ -1,21 +1,21 @@
 let sucesiones = 0;
-let anterior = 0;
-let cadenaCompleta = '0,1,';
-let x = 0;
-let y = 1;
+let cadenaCompleta = '0, 1, ';
+let var1 = 1;
+let var2 = 1;
 
 while(true){
     sucesiones = prompt('Ingresa el número de secesiones (entre 2 y 15)');
-    if (sucesiones < 3 ||  sucesiones > 15) {
+    if (sucesiones < 3 || sucesiones > 15) {
         alert('Error: Ingresa solo números entre 3 y 15')
     } else {
         break;
     }
 }
 
- for (let i = 0; i < sucesiones; i++) {
-    cadenaCompleta += x+',';
-    x = anterior+y;
-    anterior = x;
- }
- document.write(cadenaCompleta);
+for (let i = 0; i < sucesiones; i++) {
+    cadenaCompleta += (i+1) == sucesiones? var2: var2 + ', ';
+    var2 = var1 + var2;
+    var1 = var2 - var1;
+}
+
+document.write(cadenaCompleta);
