2a9268fcc65f3fc95be0f665c42a1fc7bfcc631a
nicolasar-lagash <nicolasar@lagash.com>
2019-11-08 18:11:54 -0300

    Ejercicio 11 terminado

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -9,4 +9,20 @@ var persona = {
 do{
     nombre = prompt("Ingrese el nombre"); 
 
+    if(nombre == "Salir" && contador >= 4){
+        alert("El nombre de la edad mayor es " + persona.nombre);
+        salir = true;
+    }else{
+        edad = prompt("Ingrese la edad");
+        if(contador == 0){
+            persona.nombre = nombre;
+            persona.edad = edad;
+        }else{
+            if(parseInt(persona.edad) < parseInt(edad)){
+                persona.edad = edad;
+                persona.nombre = nombre;
+            }
+        }
+        contador++;
+    }
 }while(salir == false);
