6b05acd4097652686485c6c887cf4f18cdc20a8b
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-08 16:09:39 -0300

    finaliza ejercicio8

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,24 +1,58 @@
 var persona = new Object ();
 
 persona.nombre = prompt("Ingrese su nombre");
-persona.apellido = prompt("Ingrese su apellido");
-persona.nombreCompleto = `${persona.nombre} ${persona.apellido}`;
 
-//alert(persona.nombre)
-//65-90
-//97-122
-
-do 
+while(!isNaN(persona.nombre))
 {
     persona.nombre = prompt("Error, ingrese su nombre");
+    console.log(!isNaN(persona.nombre))
 }
-while(!isNaN(persona.nombre))
+
+
+persona.apellido = prompt("Ingrese su apellido");
+
+while(!isNaN(persona.apellido)) 
+{
+    persona.apellido = prompt("Error, ingrese su apellido");
+}
+
+persona.nombreCompleto = `${persona.nombre} ${persona.apellido}`;
 
 persona.edad = prompt("Ingrese su edad");
-do
+
+while(isNaN(persona.edad))
 {
     persona.edad = prompt("Error, Ingrese su edad"); 
 }
-while(isNaN(persona.edad))
 
-alert(persona.edad)
+persona.profesion = prompt("Ingrese su profesión");
+
+while(!isNaN(persona.profesion)) 
+{
+    persona.profesion = prompt("Error, ingrese su profesión");
+}
+
+var opcion = prompt("Ingrese una de las siguientes opciones para mostrar en pantalla:\n 0:nombre\n 1:apellido\n 2:nombre completo\n 3:edad\n 4:profesión");
+
+switch (opcion)
+{
+    case 0:
+        alert(persona.nombre)
+        break;
+    
+    case "1":
+        alert(persona.apellido)
+        break;
+        
+    case "2":
+        alert(persona.nombreCompleto)
+        break;
+        
+    case "3":
+        alert(persona.edad)
+        break;
+                
+    case "4":
+        alert(persona.profesion)
+        break;
+}
