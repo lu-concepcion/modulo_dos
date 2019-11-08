f737b89e60b6ce3dc3c69cebdbb34ac73da237b1
estebanl-lagash <estebanl@lagash.com>
2019-11-08 03:57:11 -0300

    Incorporacion ejercicios 1, 2 y 3 - ejercicio 4 vacio

---
+++ Ejercicio1/index.js
@@ -0,0 +1,9 @@
+//variable edad guardará lo introducido en el prompt
+var n1 = prompt("Introduce un Numero");
+var n2 = prompt("Ingrese otro numero");
+
+var n3 = parseInt (n1) + parseInt (n2);
+
+// levanta una ventana con la edad ingresada
+alert("El resultado es "+n3)
+
