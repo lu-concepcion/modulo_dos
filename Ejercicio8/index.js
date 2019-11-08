4bacbf2a0b00ebd9c4b590d2a4cd113e6eb6ea67
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 16:06:42 -0300

    ejercicio 8 listo

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,10 +1,47 @@
-var persona = {
-    nombre:""
-    apellido:""
-    edad:0
-    profesion:""
+var persona = Object()
+var info =""
+var flag = 1
+do 
+{
+    persona.nombre= prompt("ingrese nombre")
 }
-persona.nombre= prompt("ingrese nombre")
-persona.apellido= prompt("ingrese apellido")
-persona.edad= prompt("ingrese edad")
+while(!isNaN(persona.nombre))
+do
+{
+    persona.apellido= prompt("ingrese apellido")
+}
+while(parseInt(persona.apellido))
+do
+{
+    persona.edad= prompt("ingrese edad") 
+}
+while(isNaN(persona.edad))
+do 
+{
 persona.profesion= prompt("ingrese profesion")
+}
+while(parseInt(persona.apellido))
+do{
+info= prompt("¿que informacion desea ver?")
+switch(info)
+{
+    case "nombre":
+    alert(persona.nombre)
+    flag = 0;
+    break
+    case "apellido":
+    alert(persona.apellido)
+    flag = 0;
+    break
+    case "edad":
+    alert(persona.edad)
+    flag = 0;
+    break
+    case "profesion":
+    alert(persona.profesion)
+    flag = 0;
+    break
+    default:
+    alert ("expresion invalida, intente nuevamente")
+}
+}while(flag)
