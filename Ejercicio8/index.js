3a32aecd20ce0eb530b57673681d8ab8c78dc06d
nicolasar-lagash <nicolasar@lagash.com>
2019-11-08 15:47:40 -0300

    Ejercicio 8 completo sin mostrar informacion
    Se terminan los siguientes do while pero aun no pregunta si desea mostrar la informacion

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -9,4 +9,18 @@ do{ //Nombre
     persona.nombre = prompt("Ingrese su nombre");
 }while(parseInt(persona.nombre));
 
+do{ //Apellidos
+    var apellidoA = prompt("Primer apellido");
+    var apellidoB = prompt("Segundo apellido");
+
+    persona.apellidos = apellidoA + " " + apellidoB;
+}while(parseInt(persona.apellidos));
+
+do{ //Edad
+    persona.edad = prompt("Ingrese su edad");
+}while(!parseInt(persona.edad));
+
+do{ //Profesión
+    persona.profesion = prompt("Ingrese su profesión");
+}while(parseInt(persona.profesion));
 
