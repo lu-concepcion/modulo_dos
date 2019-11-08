327cf93dad25e756dde2ea9e3131f74f361c415d
melinag <melinag@lagash.com>
2019-11-08 16:01:08 -0300

    Se añade Ejercicio 8 terminado

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,2 +1,26 @@
+//Escribir un algoritmo solicite la siguiente información de una persona:
+//Nombre: debe aceptar solo texto
+//Apellidos: debe aceptar solo texto
+//Edad: debe aceptar solo número
+//Profesión: debe aceptar solo texto
+//Mostrar información por pantalla (a elección)
+//Utilizar OBJETOS
 
+var persona = {};
+do{
+persona.nombre = prompt("ingrese nombre: ")
+}while(!isNaN(persona.nombre));
 
+do{
+persona.apellido = prompt("ingrese apellido: ")
+}while(!isNaN(persona.apellido));
+
+do{
+persona.edad = prompt("ingrese edad: ")
+}while(!Number.isInteger(parseInt(persona.edad) ));
+
+do{
+persona.profesión= prompt("ingrese Profesión: ")
+}while(!isNaN(persona.profesión));
+
+alert( persona.nombre + " " + persona.apellido + " " + persona.edad +" "+ persona.profesión);
