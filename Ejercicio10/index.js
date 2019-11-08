a5762c3b8f9e229275ba3676d4c3d951da9db636
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-08 17:26:36 -0300

    creé el array de objetos

---
+++ Ejercicio10/index.js
@@ -0,0 +1,46 @@
+var romanos = [{
+    id:1,
+    simbolo:"I"
+},
+{
+    id:2,
+    simbolo:"II"
+},
+{
+    id:3,
+    simbolo:"III"
+},
+{
+    id:4,
+    simbolo:"IV"
+},
+{
+    id:5,
+    simbolo:"V"
+},
+{
+    id:6,
+    simbolo:"VI"
+},
+{
+    id:7,
+    simbolo:"VII"
+},
+{
+    id:8,
+    simbolo:"VIII"
+},
+{
+    id:9,
+    simbolo:"IX"
+},
+{
+    id:10,
+    simbolo:"X"
+}]
+var ingresado
+var flag
+do{
+    ingresado = prompt("ingrese un numero romano")
+
+}while(flag) 
