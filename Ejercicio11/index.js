c3b5d797d41fc82bc670f00fd656d668ca057dc8
marcoac001 <marcoac001@lagash.com>
2019-11-08 18:34:56 -0300

    Agregué mi ciclo Do While con el cual puede realizar toda la ejecucion

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -8,3 +8,29 @@ var persona = {
     }
 };
 
+flag=1
+do{
+    var edad = prompt("Ingrese la edad de la persona");
+    if(edad.toLowerCase()=="salir"&&contador>=4){
+        alert("La persona con mayor edad es: "+persona.mayor.nombre)
+        flag=0;
+    }else{
+        if(!isNaN(edad)){
+            persona.edad = edad;
+            var nombre = prompt("Ingrese el nombre de esa persona");
+            if(nombre=="Salir"||nombre=="salir"&&contador>=4){
+                alert("La persona con mayor edad es: "+persona.mayor.nombre)
+                flag=0;
+            }else{
+                persona.nombre= nombre
+                contador++;
+                if(isNaN(nombre)==false){
+                    if(persona.edad>edad){
+                        edad= persona.edad;
+                        persona.mayor.nombre = persona.nombre;
+                    }
+                }
+            }
+        }
+    }
+}while(flag==1)
