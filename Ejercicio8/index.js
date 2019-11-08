d8c6068e0dd8eaeca817cade598bdd8b888aa747
estebanl-lagash <estebanl@lagash.com>
2019-11-08 16:12:56 -0300

    actualizacion ejercicio 8 - tercera parte - final

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,8 +1,55 @@
-
+count = 0
+do{
 var perNom = prompt("Ingrese el nombre de una persona");
+
+if (isNaN(parseInt(perNom)) == true){
+
+    count++
+
+}else{
+
+    alert ("Debe ingresar solo texto");
+
+}
+
+}while(count < 1);
+do{
 var perApe = prompt("Ingrese su apellido");
+if (isNaN(parseInt(perApe)) == true){
+
+    count++
+
+}else{
+
+    alert ("Debe ingresar solo texto");
+
+}
+}while(count < 2);
+do{
 var perEdad = prompt("Ingrese la edad de la persona");
+if (isNaN(parseInt(perEdad)) == false){
+
+    count++
+
+}else{
+
+    alert ("Debe ingresar solo Números");
+
+}
+}while(count < 3);
+do{
 var perPro = prompt("Ingrese la profesion de la persona");
+if (isNaN(parseInt(perPro)) == true){
+
+    count++
+
+}else{
+
+    alert ("Debe ingresar solo texto");
+
+}
+}while(count < 4);
+
 
 var persona={
 
