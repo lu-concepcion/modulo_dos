c2a213c1df5d8a3cdd8f5dd9e660c9e6ffb3bb67
marcoac001 <marcoac001@lagash.com>
2019-11-08 16:29:06 -0300

    Se agregán espacios al codigo y se termina con todos sus requisitos

--- Ejercicio9/index.js
+++ Ejercicio9/index.js
@@ -4,62 +4,62 @@ año = "";
 color = "";
 var vehiculo = new Object();
 
-do{
-   var año = prompt ("Ingrese al año del vehiculo");
-}while(Number.isNaN(parseInt(año)));
+do {
+    var año = prompt("Ingrese al año del vehiculo");
+} while (Number.isNaN(parseInt(año)));
 vehiculo.año = año;
 
-do{
+do {
     var marca = prompt("Ingrese la marca del vehiculo");
-}while(Number.isNaN(parseInt(marca))==false);
+} while (Number.isNaN(parseInt(marca)) == false);
 vehiculo.marca = marca;
 
-do{
+do {
     var modelo = prompt("Ingrese el modelo del auto");
-}while(Number.isNaN(parseInt(modelo))==false);
+} while (Number.isNaN(parseInt(modelo)) == false);
 vehiculo.modelo = modelo;
-do{
-var elegir_color = prompt("Como desea ingresar su color \n1.- HTML \n2.- Por su nombre \n3.- Ambos")
-}while(parseInt(elegir_opcion)<1||parseInt(elegir_opcion)>3);
-if(elegir_color==1){
-    do{
-var color = prompt("Ingrese el color del auto(Incluya el #):");
-    }while(String.length(color)>7||String.length(color)<0);
+do {
+    var elegir_color = prompt("Como desea ingresar su color \n1.- HTML \n2.- Por su nombre \n3.- Ambos")
+} while (parseInt(elegir_opcion) < 1 || parseInt(elegir_opcion) > 3);
+if (elegir_color == 1) {
+    do {
+        var color = prompt("Ingrese el color del auto(Incluya el #):");
+    } while (String.length(color) > 7 || String.length(color) < 0);
     vehiculo.color = color;
-}else{
-    if(elegir_color==2){ 
-    do{
-        var color = prompt("Ingrese el color del auto:");
-    }while(Number.isNaN(parseInt(modelo))==false);
-    vehiculo.color=color;
-    }else{
-        do{
+} else {
+    if (elegir_color == 2) {
+        do {
+            var color = prompt("Ingrese el color del auto:");
+        } while (Number.isNaN(parseInt(modelo)) == false);
+        vehiculo.color = color;
+    } else {
+        do {
             var color1 = prompt("Ingrese el color del auto en HTML(Incluya el #):");
-        }while(color1.length>7||color1.length<0);
-        vehiculo.color_html=color1;
-            var color2 = prompt("Ingrese el color del auto por su nombre ");
-        vehiculo.color_nombre=color2;
+        } while (color1.length > 7 || color1.length < 0);
+        vehiculo.color_html = color1;
+        var color2 = prompt("Ingrese el color del auto por su nombre ");
+        vehiculo.color_nombre = color2;
     }
 }
 var elegir_opcion = prompt("¿Que desea ver? \n1.- Año \n2.- Marca \n3.- Modelo \n4.- Color \n5.- Todo");
-if(elegir_opcion==1){
+if (elegir_opcion == 1) {
     alert(vehiculo.año)
-}else{
-    if(elegir_opcion==2){
+} else {
+    if (elegir_opcion == 2) {
         alert(vehiculo.marca)
-    }else{
-        if(elegir_opcion==3){
+    } else {
+        if (elegir_opcion == 3) {
             alert(vehiculo.modelo)
-        }else{
-            if(elegir_opcion==4){
+        } else {
+            if (elegir_opcion == 4) {
                 alert(vehiculo.color)
-            }else{
-                if(elegir_color==1||elegir_color==2){
-                alert("Año: "+vehiculo.año+"\nMarca: "+vehiculo.año+"\nModelo: "+vehiculo.modelo+"\nColor: "+vehiculo.color)
-            }else{
-                alert("Año: "+vehiculo.año+"\nMarca: "+vehiculo.año+"\nModelo: "+vehiculo.modelo+"\nColor HTML: "+vehiculo.color_html +" Color: "+vehiculo.color_nombre);
+            } else {
+                if (elegir_color == 1 || elegir_color == 2) {
+                    alert("Año: " + vehiculo.año + "\nMarca: " + vehiculo.año + "\nModelo: " + vehiculo.modelo + "\nColor: " + vehiculo.color)
+                } else {
+                    alert("Año: " + vehiculo.año + "\nMarca: " + vehiculo.año + "\nModelo: " + vehiculo.modelo + "\nColor HTML: " + vehiculo.color_html + "\nColor por nombre: " + vehiculo.color_nombre);
+                }
             }
-        }
         }
     }
 }
