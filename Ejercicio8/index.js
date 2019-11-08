8aceb9f5879e0453c1bf97c1995d56ed9dc8f596
felipep-lagash <felipep@lagash.com>
2019-11-08 15:01:48 -0300

    Se crearon variables iniciales y se solicita información a usuario

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -1,1 +1,10 @@
+let nombre = ""
+let apellido = ""
+let edad = ""
+let profesion = ""
+
+nombre = prompt('Ingresa nombre');
+apellido = prompt('Ingresa apellido');
+edad = prompt('Ingresa edad');
+profesion = prompt('Ingresa profesión');
 
