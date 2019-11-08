f9c9756261baeba2940ca82e708eff727a30d2ee
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-08 15:44:43 -0300

    ingresa validaciones para los demás items

--- Ejercicio8/index.js
+++ Ejercicio8/index.js
@@ -3,13 +3,22 @@ var persona = new Object ();
 persona.nombre = prompt("Ingrese su nombre");
 persona.apellido = prompt("Ingrese su apellido");
 persona.nombreCompleto = `${persona.nombre} ${persona.apellido}`;
-``
+
 //alert(persona.nombre)
 //65-90
 //97-122
-persona.edad = prompt("Ingrese su edad");
-if (isNaN(persona.edad))
+
+do 
 {
-    persona.edad = prompt("Error, Ingrese su edad");
+    persona.nombre = prompt("Error, ingrese su nombre");
 }
+while(!isNaN(persona.nombre))
+
+persona.edad = prompt("Ingrese su edad");
+do
+{
+    persona.edad = prompt("Error, Ingrese su edad"); 
+}
+while(isNaN(persona.edad))
+
 alert(persona.edad)
