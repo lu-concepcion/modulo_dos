6a948f2587ee9d0f5e85e9f6df8f456dfcf3609b
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-11 17:10:30 -0300

    Index del ejercicio del palindromo

--- Funciones/Ejercicio5/index.js
+++ Funciones/Ejercicio5/index.js
@@ -3,3 +3,6 @@
 // Ejemplo: ANILINA
 
 // Utilizar Funciones
+import { esPalindromo } from '../funciones.js'
+let texto = prompt("Ingrese una palabra para verificar si es palindromo")
+alert(esPalindromo(texto))
