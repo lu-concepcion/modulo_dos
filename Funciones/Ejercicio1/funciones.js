ad9ebb7e08efdf1892b733dc30e6048b42ffb04c
estebanl-lagash <estebanl@lagash.com>
2019-11-11 15:34:35 -0300

    cambio carpeta e incorporacion ejercicio 1

---
+++ Funciones/Ejercicio1/funciones.js
@@ -0,0 +1,19 @@
+
+const sumar = (num1, num2) =>{
+
+return num1 + num2
+
+}
+
+export const restar = (num1, num2) =>{
+
+return num1 - num2
+
+}
+
+export {
+
+    sumar
+
+}
+
---
+++ Funciones/Ejercicio1/index.js
@@ -0,0 +1,7 @@
+
+import {sumar, restar} from './funciones.js';
+
+    alert (sumar(1,2))
+    alert (restar(1,2))
+
+
