7afaa13baed5700fb19ecb33c81f928a4fede5f4
felipep-lagash <felipep@lagash.com>
2019-11-11 16:21:33 -0300

    Se creó estructura principal para archivo index.js

--- Funciones/Ejercicio4/index.js
+++ Funciones/Ejercicio4/index.js
@@ -1,2 +1,10 @@
+import { esTexto } from '../funciones.js';
 
+let texto = '';
+
+texto = prompt('Ingrese una palabra por favor');
+
+if (esTexto(texto)) {
+    alert('El texto contiene ' + cuentaVocales(texto) + ' vocales');
+}
 
