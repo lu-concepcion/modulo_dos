bd67ae500ca24095d4474026ea46ca49ae35035a
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-11 02:50:47 -0300

    ejercicio11 avanzando me falta

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -1,5 +1,33 @@
-// variable edad guardará lo introducido en el prompt
-var edad = prompt("Introduce tu mensaje");
+/*Escribir un algoritmo que solicite por teclado edad y nombre de una persona,
+ para finalizar debo ingresar el texto: “Salir” e indicarme el nombre de la 
+ persona de mayor edad.
 
-// levanta una ventana con la edad ingresada
-alert(edad)
+Al menos se deben ingresar 4 nombres
+
+Utilizar OBJETOS*/
+var persona= {};
+var edad;
+var nombre ;
+var contador=0;
+var r=true;
+edad = persona.edad;
+nombre = persona.nombre;
+
+do{
+   
+    persona.edad = parseInt(prompt("Ingrese Edad: "));
+    persona.nombre = prompt("Ingrese Nombre: ");
+    contador++;
+    document.write("",contador);
+    
+    if(contador==4 ){
+        r=true;
+        break;
+
+     }
+} while(r==true);
+    
+  
+
+
+
