ed4cb6f81a58b2576aa3ec707437650725e21a0d
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-11 03:35:44 -0300

    ejercicio11 terminado

--- Ejercicio11/index.js
+++ Ejercicio11/index.js
@@ -3,12 +3,20 @@ var persona = [
         nombre:"",
         edad:"0",
     }]
-var edadEntrada = 0
+var mayor = 0
 var cont = 0
 do{
     var nombre = prompt("ingrese un nombre")
+    if (nombre!="salir")
+    {
     var edad = prompt("ingrese una edad")
     persona.push({ nombre: nombre, edad: edad })
     cont++
+    }
+    if (persona[cont].edad>persona[cont-1].edad)
+    {
+        mayor=persona[cont].nombre
+    }
     console.log(persona)
-}while(cont<5||entrada!="salir")
+}while(cont<5||nombre!="salir")
+alert(mayor)
