e58d35786ee8117284fd98e9f7a7b8341bcf9aad
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-11 17:18:00 -0300

    Agregar funciones Ejercicio1 terminado

---
+++ Funciones/Ejercicio1/index.js
@@ -0,0 +1,37 @@
+import {texto,numero,mayuscula,minuscula } from './funciones.js'
+
+
+let valor = prompt("ingrese un valor: ");
+
+if(!numero(valor)){
+    alert("Es numero")
+}
+    else{
+        alert("No es numero")
+    }
+
+let valor2 = prompt("Ingrese un Texto: ");
+
+if(texto(valor2)){
+    alert("Es un texto");
+    let p=prompt("Ingrese que opcion mayuscula o minuscula");
+    if(p=="mayuscula"){
+        alert(mayuscula(valor2));
+    }
+    if(p=="minuscula"){
+        alert(minuscula(valor2));
+    }
+   
+
+}else{
+     alert("No es un Texto");
+   /* let p = prompt("quieres hacerlo mayuscula o minuscula: ");
+    if(p == "mayuscula"){
+        alert(mayuscula(valor2))
+    }
+    if(p=="minusculo"){
+            alert(minuscula(valor2))
+    }   }*/ 
+}
+
+
