c4737dd9aeba2264b0c51055168e2341a4e6535a
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-11-11 17:01:58 -0300

    script completo cuenta cuantas vocales hay en una palabra

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -24,16 +24,35 @@ const textoEnMayuscula = (text) => {
     if (text == "" || !isNaN(text))
     return false;
     else
-    return text.toLowerCase();
+    return text.toUpperCase();
 }
 
 const textoEnMinuscula = (text) => {
     if (text == "" || !isNaN(text))
     return false;
     else
-    return text.toUpperCase();
+    return text.toLowerCase();
+}
+
+const cuantasVocales = (text) => {
+    let contador = 0;
+        var textoString = textoEnMinuscula(text);
+        for (var i = 0; i <= textoString.length;i++) 
+        {
+            if (esUnaVocal(textoString.charAt(i)))
+            contador++;
+        }
+    return contador;
+}
+
+const esUnaVocal = (vocal) => {
+    const vocales = ['a', 'e', 'i', 'o', 'u'];
+    if (vocales.indexOf(vocal) !== -1)
+    return true;
+    else
+    return false;
 }
 
 export {
-    sumar, restar, esUnNumero, esUnTexto, textoEnMayuscula, textoEnMinuscula
+    sumar, restar, esUnNumero, esUnTexto, textoEnMayuscula, textoEnMinuscula, cuantasVocales, esUnaVocal
 }
--- funciones/ejercicio4/index.js
+++ funciones/ejercicio4/index.js
@@ -1,1 +1,8 @@
-import {} from '../funciones.js';
+import {cuantasVocales, esUnTexto} from '../funciones.js';
+var palabra = prompt("Ingresa una palabra:");
+if (!esUnTexto(palabra)) {
+    alert("Upps! Escribe correctamente una palabra!");
+}
+else {
+    alert("Encontramos "+cuantasVocales(palabra)+" vocales en tu texto");
+}
