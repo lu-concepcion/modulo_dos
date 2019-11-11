5f8b4eeac0a671a9ac04b524e447bd96555677ae
felipep-lagash <felipep@lagash.com>
2019-11-11 16:25:22 -0300

    Se creó función que cuenta vocales en funciones.js

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -29,10 +29,21 @@ const convierteMinuscula = texto => {
 //     return false;
 // }
 
+const cuentaVocales = texto => {
+    let contador = 0;
+    texto.toLowerCase();
+    for (let i = 0; i < texto.length; i++) {
+        if (texto.charAt(i) == 'a' || texto.charAt(i) == 'e' || texto.charAt(i) == 'i' || texto.charAt(i) == 'o' || texto.charAt(i) == 'u') {
+            contador++;   
+        }
+    }
+    return contador;
+}
 
 export {
     esNumero,
     esTexto,
     convierteMinuscula,
-    convierteMayuscula
+    convierteMayuscula,
+    cuentaVocales
 }
