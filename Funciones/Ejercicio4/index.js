334682a1f3ed900b795d59ec08cc0d1b73382d72
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-11 17:01:31 -0300

    agregado ejercicio4 funciones terminado

---
+++ Funciones/Ejercicio4/index.js
@@ -0,0 +1,9 @@
+// Escribir un algoritmo que identifique cuantas vocales tiene una palabra.
+
+// Utilizar método String.charAt(i), buscar en documentación;
+
+// Utilizar Funciones
+import{ cuentaVocales } from '../funciones.js';
+
+let palabra = prompt("Ingrese una palabra");
+alert(cuentaVocales(palabra));
--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -12,6 +12,23 @@ const convertirMayuscula=(numero)=>{
 const convertirMinuscula=(numero)=>{
     return numero.toLowerCase(numero);
 }
+
+const validarVocal=(letra)=>(letra == 'a' || letra=='e' || letra=='i' || letra=='o'|| letra=='u')
+
+
+//ejercicio4
+let contador=0;
+const cuentaVocales=(palabra)=>{
+    palabra = convertirMinuscula(palabra);
+    for(let i=0;i<palabra.length;i++){
+        if(validarVocal(palabra.charAt(i))){
+            contador++;
+        }
+    }
+    return contador;
+}
+
+
 export{
-    validaNumero,validaLetra,convertirMinuscula, convertirMayuscula
+    validaNumero,validaLetra,convertirMinuscula, convertirMayuscula,cuentaVocales
 }
