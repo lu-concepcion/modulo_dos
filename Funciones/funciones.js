ee989b05d5d79d1aa3ae95540f0a029a0529df2a
estebanl-lagash <estebanl@lagash.com>
2019-11-11 16:48:44 -0300

    actualizacion ejercicio 2 - parte dos - funcionalidad
    faltan restricciones

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,4 +1,3 @@
-
 const texto = (textoingresado) =>{
 
     if (isNaN(parseInt(textoingresado)) == false){
@@ -20,12 +19,23 @@ const minuscula = (textoingresado) =>{
     return textoingresado.toLowerCase()
 
 }
+const contarVocales = (textoingresado) =>{
+
+    let vocalcontada=0;
+
+    for(let i=0; i < textoingresado.length;i++){
+        if(textoingresado.charAt(i)=="a" || textoingresado.charAt(i)=="e" ||textoingresado.charAt(i)=="i" ||textoingresado.charAt(i)=="o" ||textoingresado.charAt(i)=="u"){
+            vocalcontada++
+        }
+    }
+
+    return vocalcontada
+
+}
 
 export {
 
-    texto,
-    mayuscula,
-    minuscula
+    contarVocales
 
 }
 
