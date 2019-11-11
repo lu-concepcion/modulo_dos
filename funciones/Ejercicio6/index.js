37ea04cd87560478de5b59578415dd578decdc8c
bryanm-lagash <bryanm@lagash.com>
2019-11-11 18:08:29 -0300

    Implementando logica de for

--- funciones/Ejercicio6/index.js
+++ funciones/Ejercicio6/index.js
@@ -1,2 +1,14 @@
-import {} from '../funciones.js'
+import { getNumeroRandom } from '../funciones.js'
+
+const arrayNumeros = [];
+let aleatorio;
+aleatorio = getNumeroRandom(1,41);
+let numGanadores;
+
+for(let i=1; i < 7; i++){
+    arrayNumeros[i] = prompt("Ingrese numero para posición " + i);
+    if(arrayNumeros[i] == aleatorio){
+        numGanadores = arrayNumeros[i];
+    }
+}
 
