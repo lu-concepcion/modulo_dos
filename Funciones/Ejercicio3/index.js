cf9101b8133d6cc77c91390f387e4c63ff3a13a7
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-12 16:27:25 -0300

    agregado ejercicio3 funciones terminado

--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -4,12 +4,15 @@
 // Resultado: 36%
 
 // Utilizar Funciones
-
+import{validaNumero, porcentaje} from '../funciones.js';
 let num1=0, num2=0;
+
 do{
     num1=prompt("Ingrese un numero");
 }while(!validaNumero(num1))
 
 do{
-    num2= prompt("Ingrese otro numero");
+    num2 = prompt("Ingrese otro numero");
 }while(!validaNumero(num2))
+
+alert("Resultado: "+porcentaje(num1,num2)+" %");
