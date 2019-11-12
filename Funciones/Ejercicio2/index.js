b313a608c334c3bc7c025f5abc8f04dd23d6c04b
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-12 14:45:45 -0300

    ingreso de datos ejercicio2

--- Funciones/Ejercicio2/index.js
+++ Funciones/Ejercicio2/index.js
@@ -1,2 +1,12 @@
+import
+{
+    sumar,
+    restar,
+    multiplicar,
+    dividir
+}from'../Funciones.js'
+const operando1=prompt("introduzca un numero")
+const operando2=prompt("introduzca un segundo numero")
+const opcion=prompt("que operacion desea realizar:\n 1 para sumar\n 2 para restar\n 3 para multiplicar\n 4 para dividir")
 
 
