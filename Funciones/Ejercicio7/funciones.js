3de6d7a4e7f9bede8bddca8b9a093260e8bbfbad
marcoac001 <marcoac001@lagash.com>
2019-11-12 14:37:26 -0300

    Implemento funciones que me verifiquen el rut si tiene puntos, si esta los tiene los borrará y además eliminará si es que hay un guion

--- Funciones/Ejercicio7/funciones.js
+++ Funciones/Ejercicio7/funciones.js
@@ -1,21 +1,35 @@
-export const verificarNumero = RUN =>(!isNaN(RUN))
+export const verificarNumero = RUN => (!isNaN(RUN))
 
-export const verificarSeparaciones = RUN =>{
+export const verificarSeparaciones = RUN => {
     let nuevoRUN = "";
-    for(let i=0;i<RUN.length;i++){
-        if(RUN.charAt(i)=="."){
-            RUN = RUN.split(".",3);
-            RUN = RUN.split("-");
-            }
-        }
-        for(let i=0;i<3;i++){
-            if(nuevoRUN==""){
-            nuevoRUN = RUN[i];
-            }else{
-                nuevoRUN = nuevoRUN+RUN[i];
+    let nuevoCambioDeRut = "";
+    let cambiarRUT = 0;
+    let arregloRUN = [];
+
+    for (let i = 0; i < RUN.length; i++) {
+        console.log(RUN.charAt(i))
+        if (RUN.charAt(i) == ".") {
+
+            arregloRUN = RUN.split(".");
+            cambiarRUT = 1;
         }
     }
-    return nuevoRUN
+    if (cambiarRUT == 1) {
+        for (let i = 0; i < 3; i++) {
+            if (nuevoRUN == "") {
+                nuevoRUN = arregloRUN[i];
+            } else {
+                nuevoRUN = nuevoRUN + arregloRUN[i];
+            }
+        }
+        for(let i=0;i<8;i++){
+            if(nuevoCambioDeRut==""){
+            nuevoCambioDeRut = nuevoRUN.charAt(i);
+            }else{
+                nuevoCambioDeRut = nuevoCambioDeRut + nuevoRUN.charAt(i);
+            }
+        }
+    }
+    return nuevoCambioDeRut
 }
 
-    
