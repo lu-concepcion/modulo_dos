696c0900dff9a5be4f2567478b02c3a162ad5b33
marcoac001 <marcoac001@lagash.com>
2019-11-12 13:26:05 -0300

    Se agregan las condiciones a la funcion para que funcione de manera correcta y se empieza a implementar en el index

--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -1,3 +1,12 @@
-import { sacarPorcentaje } from './funciones.js';
+import { sacarPorcentaje, verificarNumero } from './funciones.js';
 
+var pedirNumero = prompt("Ingrese un valor al que sacarle porcentaje");
+if(!verificarNumero(pedirNumero)){
+    var pedirPorcentaje = prompt ("Ingrese el porcentaje");
+    if(!verificarNumero(pedirPorcentaje)){
+
+    }
+}else{
+    alert("El valor ingresado no es un numero")
+}
 
