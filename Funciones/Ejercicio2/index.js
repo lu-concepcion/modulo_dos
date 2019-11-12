4aaed15169a2081ac17b82ca19d104e14b884df6
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-12 16:03:21 -0300

    Se crea carpeta con archivos js y html para resolver ejercicio 2

---
+++ Funciones/Ejercicio2/index.js
@@ -0,0 +1,16 @@
+// Escribir un algoritmo que reciba dos números. Y te ofrezca realizar las siguientes operaciones aritméticas:
+// Sumar
+// Multiplicar
+// Dividir (ojo con la división por cero)
+// Restar
+
+// Utilizar Funciones
+
+
+
+
+
+
+
+
+
