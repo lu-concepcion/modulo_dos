fc2ef1e8c3cc7553cc82971ff9eca251011a2c3a
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-12 15:23:23 -0300

    agregada solicitud de numeros al usuario ejercicio2 funciones

---
+++ Funciones/Ejercicio2/index.js
@@ -0,0 +1,12 @@
+// Escribir un algoritmo que reciba dos números. Y te ofrezca realizar las siguientes operaciones aritméticas:
+// Sumar
+// Multiplicar
+// Dividir (ojo con la división por cero)
+// Restar
+
+// Utilizar Funciones
+
+let num1=0, num2=0;
+
+num1=prompt("Ingrese un numero");
+num2= prompt("Ingrese otro numero");
