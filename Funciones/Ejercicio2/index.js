1f6db0b732beedf10ec25d2c9cd78edb2433503d
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-12 17:03:39 -0300

    agregados prompt y ciclos do while

--- Funciones/Ejercicio2/index.js
+++ Funciones/Ejercicio2/index.js
@@ -1,2 +1,15 @@
 import {sumar, restar, multiplicar, dividir} from '../funciones.js'; 
 
+
+
+let respuesta = "";
+let repetir = true;
+
+let numero1 = prompt("ingrese primer numero")
+let numero2 = prompt("ingrese segundo numero")
+
+do {
+    respuesta = prompt("Seleccione: \nSumar \nRestar \nMultiplicar \nDividir");
+    
+
+} while (repetir);
