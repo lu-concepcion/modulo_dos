c263db7f5ffe4674a11c1e35f2d993430b864b3c
melinag <melinag@lagash.com>
2019-11-12 16:43:00 -0300

    se añade ejercicio 2 de funciones terminado

--- funciones/ejercicio2/index.js
+++ funciones/ejercicio2/index.js
@@ -8,21 +8,25 @@
 import {suma,resta,multiplicacion,division} from '../funciones.js'; 
 let num1 = prompt("ingrese un numero: ");
 let num2 = prompt("ingrese un segundo numero: ");
-
+let operacion = "";
 do {
-    let operacion = prompt("ingrese la operación que desea realizar; suma, resta, multiplicacion o division.")
+     operacion = prompt("ingrese la operación que desea realizar; suma, resta, multiplicacion o division.");
     
     if(operacion == "suma" ){
-    alert(suma(num1,num2))
+    alert(suma(num1,num2));
+  
     }
     else if(operacion == "resta" ){
-        alert(resta(num1,num2))
+        alert(resta(num1,num2));
+  
     }
-     else if(operacion == "multiplicacion" ){
-        alert(multiplicacion(num1,num2))
+     else if(operacion == "multiplicacion"){
+        alert(multiplicacion(num1,num2));
+  
     }
       else if(operacion == "division" ){
-        alert(division(num1,num2))
+        alert(division(num1,num2));
+    
     }
 
-} while (operacion != "suma" || operacion != "resta" || operacion != "multiplicacion" || operacion != "division")
+} while (operacion != "suma" & operacion != "resta" & operacion != "multiplicacion" & operacion != "division")
