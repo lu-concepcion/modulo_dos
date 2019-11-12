5b8af132b9458c0d8c0b2820f320ea4173296e7c
marcoac001 <marcoac001@lagash.com>
2019-11-12 14:19:10 -0300

    Agrego ciclos FOR para verificar si hay puntos y asi asignarlos a otro lugar

--- Funciones/Ejercicio7/funciones.js
+++ Funciones/Ejercicio7/funciones.js
@@ -1,1 +1,21 @@
-export const verificarRUN = RUN =>(!isNaN(RUN)&&RUN.length<9)
+export const verificarNumero = RUN =>(!isNaN(RUN))
+
+export const verificarSeparaciones = RUN =>{
+    let nuevoRUN = "";
+    for(let i=0;i<RUN.length;i++){
+        if(RUN.charAt(i)=="."){
+            RUN = RUN.split(".",3);
+            RUN = RUN.split("-");
+            }
+        }
+        for(let i=0;i<3;i++){
+            if(nuevoRUN==""){
+            nuevoRUN = RUN[i];
+            }else{
+                nuevoRUN = nuevoRUN+RUN[i];
+        }
+    }
+    return nuevoRUN
+}
+
+    
--- Funciones/Ejercicio7/index.js
+++ Funciones/Ejercicio7/index.js
@@ -1,11 +1,11 @@
-import { verificarRUN } from './funciones.js';
+import { verificarNumero, verificarSeparaciones } from './funciones.js';
 let flag = 0;
 
-do{
+//do{
 var pedirRUN = prompt ("Ingrese su rut, sin puntos ni el numero despues del guion");
-if(verificarRUN(pedirRUN)){
-    alert("RUT CORRECTO")
-}else{
-    alert("RUT ingresado de manera incorrecta")
-}
-}while(flag==0)
+//if(verificarNumero(pedirRUN)){
+    alert(verificarSeparaciones(pedirRUN));
+//}else{
+    //alert("Lo ingresado no es un numero")
+//}
+//}while(flag==0)
