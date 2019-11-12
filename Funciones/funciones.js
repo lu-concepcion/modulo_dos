0a9e6957f09067f91a1d10be28bd965d16896da3
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-11 23:11:46 -0300

    Ejercicio 4 TERMINADO

--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,18 +1,59 @@
-
+// función para sumar dos variables
 const sumar = (num1, num2) => num1 + num2
 
+//validar lo que el usuario ingresa es texto o número
 const validarTexto = text => {
     if (isNaN(text)) {
         return "Ha ingresado texto"
     } else {return "ha ingresado numeros"};
-
 }
 
+//convertir texto que ingresa el usuario de MAYUSCULA a minuscula
 const textoAMinuscula = text => text.toLowerCase();
 
+//convertir texto a MAYUSCULA
 const textoAMayuscula = text2 => text2.toUpperCase();
+
+//indentificar VOCALES en frase ingresada por usuario
+const identificarVocales = text => {
+
+    let contador = 0;
+    for (let i = 0; i <text.length; i++) {
+        if (text.charAt(i) == "A") {
+            contador++;
+        }
+        if (text.charAt(i) == "E") {
+            contador++;
+        }
+        if (text.charAt(i) == "I") {
+            contador++;
+        }
+        if (text.charAt(i) == "O") {
+            contador++;
+        }
+        if (text.charAt(i) == "U") {
+            contador++;
+        }
+        if (text.charAt(i) == "a") {
+            contador++;
+        }
+        if (text.charAt(i) == "e") {
+            contador++;
+        }
+        if (text.charAt(i) == "i") {
+            contador++;
+        }
+        if (text.charAt(i) == "o") {
+            contador++;
+        }
+        if (text.charAt(i) == "u") {
+            contador++;
+        }
+    }
+    alert("Esta palabra tiene las siguientes vocales "+contador);
+}
 
 
 export{
-    sumar, validarTexto, textoAMinuscula, textoAMayuscula
+    sumar, validarTexto, textoAMinuscula, textoAMayuscula, identificarVocales
 }
--- Funciones/index.js
+++
@@ -1,5 +0,0 @@
-const numero = 4;
-
-import{sumar}from'./funciones.js';
-
-alert(sumar(1,2))
