30e41efa17e5789e564c8ed9b31eb758f0c9c408
nicolasar-lagash <nicolasar@lagash.com>
2019-11-12 13:48:01 -0300

    Funciones ejercicio 6 terminado sin archivo funciones
    Se termina ejercicio (con lotería tamaño 7)
    Falta pasarlo a funciones

--- Funciones/Ejercicio6/index.js
+++ Funciones/Ejercicio6/index.js
@@ -5,25 +5,29 @@ import{
 let listadoNumeros = [];
 let numerosGanadores = [];
 let contador = 1;
-let numero = 0;
 let salir = false;
 
-for(let i=0; i<7; i++){
+const size = 7;
+for(let i=0; i<size; i++){ //Genera números aleatorios
+    salir = true;
     do{
-        let nGanador = Math.random() * (41 - 1) + 1;
-        if(numerosGanadores.length != 0){
+        let nGanador = parseInt(Math.random() * (41 - 1) + 1);
+        if(numerosGanadores.length != 0){ //Si no hay número aún
+            numerosGanadores.push(nGanador);
+        }else{
             for(let j=0; j<numerosGanadores.length; j++){
-                if(nGanador == numerosGanadores[j]){
+                if(nGanador == numerosGanadores[j]){ //Que no se repitan
                     salir = false;
                 }
             }
-            salir = true;
+            if(salir == true){ //No se repitió
+                numerosGanadores.push(nGanador);
+            }
         }
-        numerosGanadores[i] = nGanador;
     }while(salir != true);
 }
 
-do{
+do{ //Pide números que no se repitan y >0 , <41
     do{
         salir = true;
         let numero = prompt("Numero para la lotería entre 1 y 41 (" + contador +")");
@@ -46,3 +50,33 @@ do{
         }
     }while(salir != true);
 }while(listadoNumeros.length != 6);
+
+contador = 0;
+
+let mensaje = "Los números de la lotería son: ";
+for(let i=0; i<numerosGanadores.length; i++){
+    mensaje += numerosGanadores[i];
+    if(i != (numerosGanadores.length-1)){
+        mensaje+= " - ";
+    }
+}
+
+mensaje += "\nLos números escogidos son: ";
+for(let i=0; i<listadoNumeros.length; i++){
+    mensaje+= listadoNumeros[i];
+    if(i != (listadoNumeros.length-1)){
+        mensaje+= " - ";
+    }
+}
+
+for(let i=0; i<listadoNumeros.length; i++){
+    for(let j=0; j<numerosGanadores.length; j++){
+        if(numerosGanadores[j] == listadoNumeros[i]){
+            contador++;
+            break;
+        }
+    }
+}
+mensaje += "\nCantidad de aciertos: " + contador;
+
+alert(mensaje);
