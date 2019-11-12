164ef7afaafa03be22ed5ce7b7314563de2c7720
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-12 17:14:29 -0300

    Revert "Ejercicio 2 de funciones TERMINADO"
    This reverts commit 9e54b293267ef08b3b1e959ac2281ccc949cdacb.

--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -1,8 +1,7 @@
 import {aritmetica} from'../funciones.js';
 
+let text = prompt("que operación desea realizar");
 let numero1 = prompt("ingrese el primer número");
-numero1 = parseInt(numero1);
 let numero2= prompt("ingrese el segundo número");
-numero2 = parseInt(numero2);
 
-alert(aritmetica(numero1, numero2));
+alert(aritmetica(resultado));
--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -74,27 +74,26 @@ Restar
 */
 
 const aritmetica = (numero1, numero2) => {
-    let text = prompt("que operación desea realizar");
-    if (text == "sumar") {
-       return numero1 + numero2
+    if (text == sumar) {
+       return resultado = numero1 + numero2
     }
 
-    if (text == "multiplicar") {
-        return numero1 * numero2
+    if (text == multiplicar) {
+        return resultado = numero1 * numero2
     }
 
-    if (text == "restar") {
-        return numero1 - numero2
+    if (text == restar) {
+        return resultado = numero1 - numero2
     }
 
-    if (text == "dividir") {
+    if (text == dividir) {
         while (numero1 > 0 && numero2 > 0) {
        let opcion =  prompt("si desea que el primer numero ingresado se divida por el segundo ingrese 1, de lo contrario numero 2 para la inversa");
        if (opcion == 1) {
-            return numero1 / numero2
+            return resultado = numero1 / numero2
        }
        if (opcion == 2) {
-            return numero2 / numero1
+            return resultado = numero2 / numero1
     }
   }
 }
