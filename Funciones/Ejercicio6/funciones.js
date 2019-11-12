a8d5ca32eb36b1c568cb7ae203c81d76890019f0
marcoac001 <marcoac001@lagash.com>
2019-11-12 16:38:49 -0300

    Cambie de lugar el funciones.js como me pidio Hernan

--- Funciones/Ejercicio6/funciones.js
+++
@@ -1,4 +0,0 @@
-export const numero_aleatorio = i => {
-        return Math.floor((Math.random()*36)+1)}
-
-export const verificar_numero = numero => (isNaN(numero))
--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -1,3 +1,93 @@
+export const validar_numero = (n1)=>{
+    return (isNaN(n1))
+}
+
+export const validar_texto =(t1)=>{
+    return(isNaN(t1))
+}
+export const transformar_mayuscula = (t2)=>{
+    return(t2.toUpperCase())
+}
+export const transformar_minuscula = (t3)=>{
+    return(t3.toLowerCase())
+}
+
+export const operacionesAritmeticas = (opcion, numero1, numero2) => {
+    let resultado = 0;
+    if (opcion == 1) {
+        resultado = parseInt(numero1) + parseInt(numero2);
+    } else {
+        if (opcion == 2) {
+            resultado = parseInt(numero1) - parseInt(numero2);
+        } else {
+            if (opcion == 3) {
+                resultado = parseInt(numero1) * parseInt(numero2);
+            } else {
+                if (numero2 == 0) {
+                    resultado = "No se puede realizar esta division"
+                } else {
+                    resultado = parseInt(numero1)/parseInt(numero2);
+                }
+            }
+        }
+    }
+    return resultado
+}
+
+export const verificarNumeros = (numero) => (isNaN(numero))
+
+export const sacarPorcentaje = (numero, porcentaje) => {
+    var resultado = 0;
+    if (porcentaje <= 0 || porcentaje > 100|| numero<0) {
+        resultado = "No se puede sacar un porcentaje con esos valores"
+    } else {
+        resultado = (numero * porcentaje) / 100;
+    }
+    return resultado
+}
+
+
+export const verificarNumero = (numero) =>(isNaN(numero))
+
+const verificar_vocales = letra => (letra=="a"||letra=="e"||letra=="i"||letra=="o"||letra=="u")
+
+export const contar_vocales = frase =>{
+    let contador = 0;
+    frase = frase.toLowerCase();
+    for(let i=0;i<=frase.length;i++){
+        if(verificar_vocales(frase.charAt(i))){
+            contador++;
+        }
+    }
+    return contador
+}
+
+export const voltear_palabra = palabra =>{
+    var nueva_palabra="";
+    console.log(`a: ${palabra.length}`)
+    let largo = palabra.length
+
+    for(var i=largo;i>=0;i--){
+        if(palabra.charAt(i)!==" "){
+        console.log(`b: ${palabra.charAt(i)}`)
+        if(nueva_palabra==""){
+
+        nueva_palabra= palabra.charAt(i);
+        }else{
+            nueva_palabra = nueva_palabra+palabra.charAt(i);
+        }
+    }
+}
+    return nueva_palabra
+}
+
+export const verificar_palindrome = (frase) =>(voltear_palabra(frase)==frase)
+
+export const numero_aleatorio = i => {
+    return Math.floor((Math.random()*36)+1)}
+
+export const verificar_numero = numero => (isNaN(numero))
+
 export const verificarNumero = RUN => (!isNaN(RUN))
 
 export const digitoVerificador = RUN => {
