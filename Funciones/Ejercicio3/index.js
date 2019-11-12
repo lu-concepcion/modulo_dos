1a33ca89ecf7da479c697c17a215b0291570ba66
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-12 17:37:07 -0300

    correcion ejercicio3 funciones validacion primer numero

--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -9,7 +9,7 @@ let num1=0, num2=0;
 
 do{
     num1=prompt("Ingrese un numero");
-}while(!validaNumero(num1))
+}while(!validaNumero(num1) || num1==0)
 
 do{
     num2 = prompt("Ingrese otro numero");
