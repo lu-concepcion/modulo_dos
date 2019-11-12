eaf9568315c9b0eb85ca4c10c339251a22700b0a
felipep-lagash <felipep@lagash.com>
2019-11-12 17:14:49 -0300

    Se crearon primeras variables y estructura en index.js

---
+++ Funciones/Ejercicio8/index.js
@@ -0,0 +1,7 @@
+import { encontrarElemento} from "../funciones.js";
+
+let elemento = 5;
+let arregloNumeros = [1,2,3,4,5,6,7,8,9,10];
+let arregloColores = ['rojo','blanco','verde','azul','amarillo'];
+
+console.log(encontrarElemento(elemento));
