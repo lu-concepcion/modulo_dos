075398c4cac17f42c50c2642d2d02e407c3a5326
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-12 15:13:10 -0300

    validacion de los operandos

--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -3,8 +3,12 @@ import
     porcentaje,
     esNumero
 }from'../Funciones.js'
+do{
 const total=prompt("ingrese el total");
+}while(!esNumero)
+do{
 const fraccion=prompt("ingrese la fraccion");
+}while(!esNumero)
 alert (porcentaje(total,fraccion)+"%");
 
 
