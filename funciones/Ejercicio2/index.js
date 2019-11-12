544314c2822be92c708bf762b68a96d3652ba643
bryanm-lagash <bryanm@lagash.com>
2019-11-12 15:24:30 -0300

    Ejercicio 2 semi-completo

--- funciones/Ejercicio2/index.js
+++ funciones/Ejercicio2/index.js
@@ -1,5 +1,8 @@
+import {sumar, restar, multiplicar, dividir} from '../funciones.js'
+
 let firstValor = "";
 let secondValor = "";
+let rep = false;
 
 do{
     firstValor = prompt("Ingrese primer valor");
@@ -9,3 +12,24 @@ do{
     secondValor = prompt("Ingrese segundo valor");
 }while(isNaN(parseInt(secondValor)))
 
+do{
+    const resp = prompt("Ingrese opcion : \n 1.Sumar \n 2.Restar \n 3.Multiplicar \n 4.Dividir")
+    switch(resp){
+        case "1":
+            alert("El resultado es: " + sumar(firstValor, secondValor))
+            break;
+        case "2":
+            alert("El resultado es: " + restar(firstValor, secondValor))
+            break;
+        case "3":
+            alert("El resultado es: " + multiplicar(firstValor, secondValor))
+            break;
+        case "4":
+            alert("El resultado es: " + dividir(firstValor, secondValor))
+            break;
+        default:
+            alert("Ingrese opcion valida");
+            rep = true;
+    }
+
+}while(rep)
