d49eb197ddbcb17ea797ea8cc0bfdd175e784388
marcoac001 <marcoac001@lagash.com>
2019-11-12 13:30:34 -0300

    Se agrego una validacion para el numero en la funcion sacarPorcentaje y agregué el ejercicio7

--- Funciones/Ejercicio3/funciones.js
+++ Funciones/Ejercicio3/funciones.js
@@ -1,6 +1,6 @@
 export const sacarPorcentaje = (numero, porcentaje) => {
     var resultado = 0;
-    if (porcentaje <= 0 || porcentaje > 100) {
+    if (porcentaje <= 0 || porcentaje > 100|| numero<0) {
         resultado = "No se puede sacar un porcentaje con esos valores"
     } else {
         resultado = (numero * porcentaje) / 100;
