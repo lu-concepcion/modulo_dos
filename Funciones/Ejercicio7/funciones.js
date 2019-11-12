12252e68f6f508e9d1f4a837f4d480c7c5c79323
marcoac001 <marcoac001@lagash.com>
2019-11-12 14:56:38 -0300

    Se agregan verificaciones para ambos casos

--- Funciones/Ejercicio7/funciones.js
+++ Funciones/Ejercicio7/funciones.js
@@ -7,28 +7,34 @@ export const verificarSeparaciones = RUN => {
     let arregloRUN = [];
 
     for (let i = 0; i < RUN.length; i++) {
-            if (RUN.charAt(i) == ".") {
+        if (RUN.charAt(i) == ".") {
+            arregloRUN = RUN.split(".");
+            cambiarRUT = 1;
+        }
+    }
+    if (cambiarRUT == 1) {
+        for (let i = 0; i < 3; i++) {
+            if (nuevoRUN == "") {
+                nuevoRUN = arregloRUN[i];
+            } else {
+                nuevoRUN = nuevoRUN + arregloRUN[i];
+            }
+        }
+        for (let i = 0; i < 8; i++) {
+            if (nuevoCambioDeRut == "") {
+                nuevoCambioDeRut = nuevoRUN.charAt(i);
+            } else {
+                nuevoCambioDeRut = nuevoCambioDeRut + nuevoRUN.charAt(i);
+            }
+        }
+    }
 
-                arregloRUN = RUN.split(".");
-                cambiarRUT = 1;
-            }
+    if (cambiarRUT == 0) {
+        if (RUN.length <= 8 && RUN.length > 6) {
+            nuevoCambioDeRut = "RUT CORRECTO"
+        } else {
+            nuevoCambioDeRut = "RUT INCORRECTO"
         }
-        if (cambiarRUT == 1) {
-            for (let i = 0; i < 3; i++) {
-                if (nuevoRUN == "") {
-                    nuevoRUN = arregloRUN[i];
-                } else {
-                    nuevoRUN = nuevoRUN + arregloRUN[i];
-                }
-            }
-            for (let i = 0; i < 8; i++) {
-                if (nuevoCambioDeRut == "") {
-                    nuevoCambioDeRut = nuevoRUN.charAt(i);
-                } else {
-                    nuevoCambioDeRut = nuevoCambioDeRut + nuevoRUN.charAt(i);
-                }
-            }
-        }
+    }
     return nuevoCambioDeRut
 }
-
