a48d54673ec03247970a9bf5df000314b8398013
melinag <melinag@lagash.com>
2019-11-12 17:46:52 -0300

    ejercicio 3 de funciones terminado, se agrega validacion para numeros ingresados

--- funciones/Ejercicio3/index.js
+++ funciones/Ejercicio3/index.js
@@ -5,6 +5,13 @@
 // Utilizar Funciones
  
 import {porcentaje} from '../funciones.js'; 
-let num1 = prompt("ingrese un numero: ");
-let num2 = prompt("ingrese un segundo numero: ");
+let num1 = 0
+let num2 = 0
+do {
+     num1 = prompt("ingrese un numero: ");
+} while (isNaN(num1));
+do {
+     num2 = prompt("ingrese un segundo numero: ");
+} while (isNaN(num2))
+
 alert(`El porcentaje del segundo numero respecto al primero es ${porcentaje(num1,num2)}`)
