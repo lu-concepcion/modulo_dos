6fed54432d01fe98e7fc5986245cf8c034bd5028
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-12 14:46:48 -0300

    llamada a las funciones (ejercicio2)

--- Funciones/Ejercicio2/index.js
+++ Funciones/Ejercicio2/index.js
@@ -8,5 +8,23 @@ import
 const operando1=prompt("introduzca un numero")
 const operando2=prompt("introduzca un segundo numero")
 const opcion=prompt("que operacion desea realizar:\n 1 para sumar\n 2 para restar\n 3 para multiplicar\n 4 para dividir")
+switch(opcion)
+{
+    case "1":
+    alert(sumar(operando1,operando2));
+    break;
+    case "2":
+    alert(restar(operando1,operando2));
+    break;
+    case "3":
+    alert(multiplicar(operando1,operando2));
+    break;
+    case "4":
+    alert(dividir(operando1,operando2));
+    break;
+    default:
+    alert("operacion no valida");
+    break;
+}
 
 
