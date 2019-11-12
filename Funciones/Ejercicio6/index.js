222aed6b09769050cf7b49c281fbbcf606b4c649
jeremya-lagash <jeremya@lagash.com>
2019-11-12 17:11:47 -0300

    se agrega el for que hace los resultados del loto

--- Funciones/Ejercicio6/index.js
+++ Funciones/Ejercicio6/index.js
@@ -1,6 +1,17 @@
-import {numeroRandom, esNumero} from '../funciones.js'
+import {repiteNumero, numeroRandom, esNumero} from '../funciones.js'
 
 let numeros = [];
 let numAleatorios = [];
 let aleatorio = 0;
 let numeroUsuario = 0;
+
+for (let i = 0; i < 6; i++) {
+    aleatorio = numeroRandom(1,41);
+    if(repiteNumero(aleatorio, numAleatorios)) {
+        i--;
+        continue;
+    } else {
+        numAleatorios[i] = aleatorio;
+    }
+}
+
