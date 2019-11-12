14dff00ac92b15058e7e676f94c2759b802556f9
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-12 16:20:45 -0300

    Se agrega la  entrada de numeros, solicitud de operacion a realizar y la impresion de esta

--- Funciones/Ejercicio2/index.js
+++ Funciones/Ejercicio2/index.js
@@ -8,6 +8,22 @@
 
 
 
+import{ dividir, multiplicar, restar, sumar } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+
+let numero1=prompt("Ingrese el primer numero");
+let numero2=prompt("Ingrese el segundo numero");
+
+let operacion=prompt("Ingrese operación a realizar: Sumar, Restar, Multiplicar o Dividir");
+
+if (operacion=="Sumar"){
+    alert(sumar(numero1,numero2))
+} else if (operacion=="Restar"){
+    alert(restar(numero1,numero2))
+} else if (operacion=="Dividir"){
+    alert(dividir(numero1,numero2))
+} else if (operacion=="Multiplicar"){
+    alert(multiplicar(numero1,numero2))
+}
 
 
 
