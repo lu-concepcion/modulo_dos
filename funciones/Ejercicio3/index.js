b55d37c12b17eaabb98b22d18239f20821614aa5
bryanm-lagash <bryanm@lagash.com>
2019-11-12 16:09:14 -0300

    Logica basica para preguntar primer valor

---
+++ funciones/Ejercicio3/index.js
@@ -0,0 +1,12 @@
+import {sacarPorcentaje, verNum} from '../funciones.js'
+
+let num1 = "";
+let num2 = "";
+
+do{
+    num1 = prompt("Ingrese valor");
+}while(isNaN(parseInt(num1)))
+
+
+
+alert(num1)
