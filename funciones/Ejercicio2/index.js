efd1db3024df6dc9b39f78a86aa92314ec4ea3eb
bryanm-lagash <bryanm@lagash.com>
2019-11-12 15:06:41 -0300

    Se agrega logica basica para pedir valores

---
+++ funciones/Ejercicio2/index.js
@@ -0,0 +1,11 @@
+let firstValor = "";
+let secondValor = "";
+
+do{
+    firstValor = prompt("Ingrese primer valor");
+}while(isNaN(parseInt(firstValor)))
+
+do{
+    secondValor = prompt("Ingrese segundo valor");
+}while(isNaN(parseInt(secondValor)))
+
