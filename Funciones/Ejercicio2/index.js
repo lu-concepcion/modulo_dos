361f5da2a2b59030bcf51d967aababcfa0d5f98d
marcoac001 <marcoac001@lagash.com>
2019-11-12 13:15:56 -0300

    Se agregan las opciones de operaciones a la funcion funcionesAritmeticas, para implementarla en el index

--- Funciones/Ejercicio2/index.js
+++ Funciones/Ejercicio2/index.js
@@ -8,7 +8,12 @@ if (!verificarNumeros(pedir_numero1)) {
         do{
         var pedir_operacion = prompt ("¿Que operacion desea realizar?\n1.- Suma\n2.- Resta\n3.- Multiplicacion \n4.- Division")
         }while(verificarNumeros(pedir_operacion)||pedir_operacion<1&&pedir_operacion>4)
+        alert(operacionesAritmeticas(pedir_operacion, pedir_numero1, pedir_numero2))
+    }else{
+        alert("Solo se permite el ingreso de numeros")
     }
+}else{
+    alert("Solo se permite el ingreso de numeros")
 }
 
 
