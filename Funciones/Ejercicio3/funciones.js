dd12b73deb15516a9f47b0b44595fc41b11edb82
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-12 02:18:44 -0300

    avanse ejercicio3

--- Funciones/Ejercicio3/funciones.js
+++ Funciones/Ejercicio3/funciones.js
@@ -4,16 +4,21 @@ Ejemplo: ANILINA
 
 Utilizar Funciones*/
 
-let primeraletra = palabra.charAt(i);
-let ultimaletra =palabra.charAt(palabra.legth-1);
-let espacio= "";
-let nuevapalabra;
+
+
+//let espacio= "";
+
 export const validarpalindromo=(palabra)=>{
+    let nuevapalabra;
+    let primeraletra = palabra.charAt(i);
+    let ultimaletra =palabra.charAt(palabra.legth-1);
     for(let i=0;i<=palabra.legth;i++){
             if(primeraletra==ultimaletra){
                   nuevapalabra=ultimaletra;
+                  document.write(+nuevapalabra+"");
             }
+        }
         return(nuevapalabra);
-    }
+    
 }
 
--- Funciones/Ejercicio3/index.js
+++ Funciones/Ejercicio3/index.js
@@ -1,16 +1,10 @@
-import{validarpalindromo} from './funciones.js'
+import{validarpalindromo,} from './funciones.js'
 
 let palabra = prompt("Ingresa una palabra: ");
 
-     if(isNaN(validarpalindromo(palabra)))
-     if(validarpalindromo(palabra)){
-
-         alert("es una palabra palindroma")
-       
-     }else{
-          alert("No es una palabra palindroma")
-     }
+    // if(isNaN(palabra)){
+     alert(validarpalindromo(palabra))
+    
      
-
 
 
