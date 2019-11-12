b2c5cb49edfc61383777401c983fa7b693dc7d6e
felipep-lagash <felipep@lagash.com>
2019-11-12 17:41:18 -0300

    Se creó arreglo de objetos para hacer busqueda

--- Funciones/Ejercicio8/index.js
+++ Funciones/Ejercicio8/index.js
@@ -1,8 +1,29 @@
-import { encontrarElemento} from "../funciones.js";
+import { encontrarElemento, buscarEnArray} from "../funciones.js";
 
 let elemento = 11;
 let arregloNumeros = [1,2,3,4,5,6,7,8,9,10];
 let arregloColores = ['rojo','blanco','verde','azul','amarillo'];
 
 console.log('Existe elemento: ' + (encontrarElemento(elemento, arregloNumeros)?'Sí':'No'));
-console.log('test');
+
+let arregloPersonas = [
+    {
+        nombre: 'Felipe',
+        edad: 27,
+        sexo: 'M'
+    },
+    {
+        nombre: 'Valentina',
+        edad: 28,
+        sexo: 'F'
+    },
+    {
+        nombre: 'Homero',
+        edad: 38,
+        sexo: 'M'
+    }
+];
+
+console.log(arregloPersonas);
+
+
