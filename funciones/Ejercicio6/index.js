5c04ea30452e545b5a226e6c97926af2f52dbe8f
bryanm-lagash <bryanm@lagash.com>
2019-11-13 17:47:43 -0300

    Se elimina funcion "seRepite" y se hacen algunos arreglos en index.js

--- funciones/Ejercicio6/index.js
+++ funciones/Ejercicio6/index.js
@@ -1,23 +1,24 @@
-import { generaNumAleatorios,seRepite,verNum } from '../funciones.js'
+import { generaNumAleatorios,verNum,buscarElemento } from '../funciones.js'
 
 let arrayNumeros = [];
 let numero = "";
 let aleatorio;
-aleatorio = generaNumAleatorios(1,41);
+aleatorio = generaNumAleatorios(1,36);
 let numGanadores;
 
 for(let i=1; i < 7; i++){
     numero = prompt("Ingrese numero para posición " + i);
-    if(){
-        if(seRepite(numero, arrayNumeros)){
+    if(verNum(numero) == false){
+        if(buscarElemento(arrayNumeros,numero)){
             i--;
+            alert("No se pueden repetir los numeros")
             continue;
         }else {
             arrayNumeros[i] = numero;
         }
     }else{
         alert("¡Solo se pueden ingresar numeros!");
+        i--;
     }
-    
 }
 
