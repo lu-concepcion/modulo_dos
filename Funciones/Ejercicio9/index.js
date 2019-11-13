09f76832f1aee52250e02e2fe8dbdfac44766d2d
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 16:00:22 -0300

    Se incluye prueba para función que ordena de menor a mayor del ejercicio 9

--- Funciones/Ejercicio9/index.js
+++ Funciones/Ejercicio9/index.js
@@ -1,2 +1,10 @@
 
 // Escriba una función que me ordene un array, de menor a mayor, y viceversa
+
+import{ ordenarArrayMenorMayor  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+
+
+
+const Elementos=["hola",4, 3, "1", 2, "chao"];
+
+alert(ordenarArrayMenorMayor(Elementos))
