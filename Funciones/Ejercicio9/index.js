ea5336e3ef50fbafb8a7e28e74cdbfb671727f7f
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 16:56:30 -0300

    Se agrega función que ordena elementos de un array de menor a mayor para el ejercicio 9

--- Funciones/Ejercicio9/index.js
+++ Funciones/Ejercicio9/index.js
@@ -1,15 +1,13 @@
 
 // Escriba una función que me ordene un array, de menor a mayor, y viceversa
 
-//import{ ordenarArray  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+import{ ordenarArray  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
 
 
 
-const Elementos=["hola",4, 3, "1", 2, "chao"];
-
-const ordenar =prompt("Para ordenar de menor a mayor ingrese 1 y de mayor a menor ingrese 2")
+const elementos=[4, 3, 1, 2, 10, 17,33,2];
 
 
 
 
-alert(ordenarArray(Elementos, ordenar))
+alert(ordenarArray(elementos))
