c27854bc0aeb00c2e2c5b60b26dac18efc65823c
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-13 15:03:07 -0300

    ejercicio6 calculo de aciertos listo

--- Funciones/Ejercicio6/index.js
+++ Funciones/Ejercicio6/index.js
@@ -7,10 +7,12 @@ let entrada=[0,0,0,0,0,0]
 let contador=0
 do{
 entrada[contador] = prompt("ingrese un numero")
-if(entrada[contador]>=1 && entrada[contador]<=41)
+if(entrada[contador]>="1" && entrada[contador]<="41")
 {
     contador++
 }
 }while(contador<6)
-alert (entrada)
-
+let ganadores= []
+ganadores = sorteo(1,41)
+alert(aciertosSorteo(ganadores,entrada))
+alert (ganadores+"-"+entrada)
