8444ee0f3df97e270291dac89a04456e0187c696
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-13 15:13:46 -0300

    elimina codigo comentado

--- Funciones/Ejercicio7/index.js
+++ Funciones/Ejercicio7/index.js
@@ -6,12 +6,6 @@ let check = esNumero(run);
 while(check != 1)
 {
     run = prompt("Los carácteres ingresados no corresponden a numeros, por favor ingrese nuevamente")
-    // check = esNumero(run);
-    // if (check == 0)
-    // {
-    //     alert("El dígito verificador de su run es: "+RUN(run))
-    // }
 }
-
 
 alert("El dígito verificador de su run es: "+RUN(run))
