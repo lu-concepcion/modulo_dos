931496a2ae45c3daa0f9e3bce468f23b95f40ebd
bryanm-lagash <bryanm@lagash.com>
2019-11-13 17:29:55 -0300

    Se implementa logica para que no se repitan numeros ingresados

--- funciones/Ejercicio6/index.js
+++ funciones/Ejercicio6/index.js
@@ -1,14 +1,23 @@
-import { getNumeroRandom } from '../funciones.js'
+import { generaNumAleatorios,seRepite,verNum } from '../funciones.js'
 
-const arrayNumeros = [];
+let arrayNumeros = [];
+let numero = "";
 let aleatorio;
-aleatorio = getNumeroRandom(1,41);
+aleatorio = generaNumAleatorios(1,41);
 let numGanadores;
 
 for(let i=1; i < 7; i++){
-    arrayNumeros[i] = prompt("Ingrese numero para posición " + i);
-    if(arrayNumeros[i] == aleatorio){
-        numGanadores = arrayNumeros[i];
+    numero = prompt("Ingrese numero para posición " + i);
+    if(){
+        if(seRepite(numero, arrayNumeros)){
+            i--;
+            continue;
+        }else {
+            arrayNumeros[i] = numero;
+        }
+    }else{
+        alert("¡Solo se pueden ingresar numeros!");
     }
+    
 }
 
