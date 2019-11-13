d92f9e03c3f52c2feb196ba8ef6eda267a106c2c
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 18:07:30 -0300

    Se incluye prueba para el ejercicio 11

--- Funciones/Ejercicio11/index.js
+++ Funciones/Ejercicio11/index.js
@@ -2,3 +2,14 @@
 // Escriba un algoritmo que extraiga de un texto, todo aquello que se encuentre entre paréntesis, ej:
 
 // Hola (mundo) que (tal) estas -> mundo tal 
+
+import{ extraerTexto  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+
+
+
+let texto="Hola (mundo) q(ue (tal) estas( 34 (2)(4) hgh";
+
+
+
+
+alert(extraerTexto(texto))
