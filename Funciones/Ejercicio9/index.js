385092d0a2f94db0ba9c12426d972da17431a696
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 16:19:14 -0300

    Ejercicio 9: se incluye prueba para ordenar array

--- Funciones/Ejercicio9/index.js
+++ Funciones/Ejercicio9/index.js
@@ -1,10 +1,15 @@
 
 // Escriba una función que me ordene un array, de menor a mayor, y viceversa
 
-import{ ordenarArrayMenorMayor  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+//import{ ordenarArray  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
 
 
 
 const Elementos=["hola",4, 3, "1", 2, "chao"];
 
-alert(ordenarArrayMenorMayor(Elementos))
+const ordenar =prompt("Para ordenar de menor a mayor ingrese 1 y de mayor a menor ingrese 2")
+
+
+
+
+alert(ordenarArray(Elementos, ordenar))
