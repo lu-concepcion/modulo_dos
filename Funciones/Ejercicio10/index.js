46a2212df0a5f5ada761812110d695cfc0ceb47f
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 17:00:28 -0300

    Se crea carpeta con archivos para resolver ejercicio 10

---
+++ Funciones/Ejercicio10/index.js
@@ -0,0 +1,13 @@
+
+// Escriba una función que me ordene un array, de menor a mayor, y viceversa
+
+import{ ordenarArray  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
+
+
+
+const elementos=[4, 3, 1, 2, 10, 17,33,2];
+
+
+
+
+alert(ordenarArray(elementos))
