7829fee271d0cba60872d1538d9898a0b125b8e6
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-13 17:09:45 -0300

    Se agrega: mostrar resultado al ejercicio 10

--- Funciones/Ejercicio10/index.js
+++ Funciones/Ejercicio10/index.js
@@ -1,4 +1,3 @@
-// Escriba un algoritmo que me calcule el factorial de un número. Ej: 5! = 1 * 2 * 3 * 4 * 5 = 120
 
 import{ factorial  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
 
