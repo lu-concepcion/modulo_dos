064bc3a3714969973aff6a1cb0e35e2d82629670
melinag <melinag@lagash.com>
2019-11-13 15:07:57 -0300

    Se añade pequeño avance a ejercicio 6

--- funciones/Ejercicio6/index.js
+++ funciones/Ejercicio6/index.js
@@ -1,3 +1,10 @@
-import {palindromo} from '../funciones.js'; 
-let texto = prompt("ingrese texto: ");
-alert(palindromo(texto));
+// Escribir un algoritmo que genere un sorteo del juego de azar LOTO, para esto, debe solicitar 6 números al usuario, posterior a eso, dar la opción de cambiar los números elegidos, o realizar sorteo. El resultado debe mostrar cuantos aciertos tuvo la persona.
+// Utilizar Math.random(), ver documentación.
+// Utilizar Funciones
+import {} from '../funciones.js'; 
+
+let numerosIngresados =[]
+for (let i = 0; i < 6; i++) {
+    numerosIngresados[i] = prompt(`ingrese numero ${i+1}`)    
+}
+ alert(numerosIngresados);
