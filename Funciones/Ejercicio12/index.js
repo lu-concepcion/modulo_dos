e05e142322e657faeaeea727a58525e67808b08e
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-14 15:42:13 -0300

    creacion del array de personas

---
+++ Funciones/Ejercicio12/index.js
@@ -0,0 +1,18 @@
+import{
+    mayoresQue
+}from'../Funciones.js'
+personas [
+    {
+        nombre:"Pedro",
+        fechaDeNacimiento: new Date(1996,8,11)
+    },
+    {
+        nombre:"Juan",
+        fechaDeNacimiento: new Date(2004,3,14)
+    },
+    {
+        nombre:"Diego",
+        fechaDeNacimiento: new Date(1987,5,21)
+    }
+
+]
