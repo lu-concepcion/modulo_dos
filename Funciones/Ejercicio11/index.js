bc95d2c9cbacb9b8d288444d7cf6a26197e72cf9
felipep-lagash <felipep@lagash.com>
2019-11-14 12:24:54 -0300

    Se cambiaron datos de pruebas en index.js por ingreso de información de usuario

--- Funciones/Ejercicio11/index.js
+++ Funciones/Ejercicio11/index.js
@@ -1,28 +1,11 @@
 import {textoEnParentisis, esTexto} from '../funciones.js';
 
 
-// while(true) {
-//     let cadena = prompt('Ingrese texto por favor')
-//     if (esTexto(cadena)) {
-//         alert('Resultado: ' + textoEnParentisis(cadena))
-//         break;
-//     }
-//     alert('Error: solo ingresa texto');
-// }
-
-let cadena1 = 'hola (mundo) que (tal)';
-let cadena2 = 'hola (aa(hola)';
-let cadena3 = '()';
-let cadena4 = '()(as';
-let cadena5 = '()(as)(';
-console.log(cadena1);
-console.log(cadena2);
-console.log(cadena3);
-console.log(cadena4);
-console.log(cadena5);
-console.log('resultado-------------------------------------------------------');
-console.log(textoEnParentisis(cadena1));
-console.log(textoEnParentisis(cadena2));
-console.log(textoEnParentisis(cadena3));
-console.log(textoEnParentisis(cadena4));
-console.log(textoEnParentisis(cadena5));
+while(true) {
+    let cadena = prompt('Ingrese texto por favor')
+    if (esTexto(cadena)) {
+        alert('Resultado: ' + textoEnParentisis(cadena))
+        break;
+    }
+    alert('Error: solo ingresa texto');
+}
