2b90b8e193263c9b0c04989834cd07586d31cbc7
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-14 14:45:18 -0300

    Se modificó el arreglo a ordenar para probar la función

--- Funciones/Ejercicio9/index.js
+++ Funciones/Ejercicio9/index.js
@@ -1,11 +1,11 @@
 
-// Escriba una función que me ordene un array, de menor a mayor, y viceversa
+// Escriba una función que me ordene un array, de menor a mayor
 
 import{ ordenarArray  } from '../funciones.js';  // importar sin nombre {} para que despues sugiera
 
 
 
-const elementos=[4, 3, 1, 2, 10, 17,33,2];
+const elementos=[24,3,10,5,4, 2, 3, 1, 2, 10, 17,33,2];
 
 
 
