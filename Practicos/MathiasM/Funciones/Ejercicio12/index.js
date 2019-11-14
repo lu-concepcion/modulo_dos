d752dc7f65cf3134278c99a617c5c375f286b846
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-14 16:14:46 -0300

    Restructura de carpetas

---
+++ Practicos/MathiasM/Funciones/Ejercicio12/index.js
@@ -0,0 +1,11 @@
+// Escriba un programa que me muestre, de un listado de personas, solo
+// a aquellas que sean mayores a X edad, el objeto no puede tener como
+// propiedad la edad, solo la fecha de nacimiento.
+// // new Date(año, mes, dia)
+// const fecha = new Date("1990","2","5");
+
+// fecha.getDay() // retorna el número del día
+// fecha.getMonth() // retorna el número del mes
+// fecha.getFullYear() // retorna el año
+
+let personas = [{nombre:"mathias", fechaNacimiento:"1997/"}]
