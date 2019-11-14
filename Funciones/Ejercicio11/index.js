b09f9117ea43b8e51c74f0185f4efe4e3302149c
fabianb-lagash <fabianb@lagash.com>
2019-11-14 15:12:03 -0300

    Se agrega ejercicio11 para trabajar

---
+++ Funciones/Ejercicio11/index.js
@@ -0,0 +1,50 @@
+import { buscarNumeroTexto,buscarEnArray } from '../funciones.js';
+
+
+
+/*
+let array = [1,2,3,4,5,6,7,8]
+let elemento = 100;
+
+console.log("funcion 1")
+console.log(buscarNumeroTexto(array,elemento))
+
+let arrayPersonas = [
+    {
+        id: 1,
+        nombre: "Máximo",
+        edad: 6,
+        sexo: "M"
+    },
+    {
+        id: 2,
+        nombre: "Camila",
+        edad: 25,
+        sexo: "F"
+    }
+]
+
+console.log("funcion 2")
+console.log(buscarEnArray(arrayPersonas,"nombre", "Máximo"))
+
+console.log("funcion 3")
+console.log(buscarEnArray(arrayPersonas,"sexo", "F"))
+*/
+
+
+//empezamos a crear los objetos
+
+// vehiculo  = new Object()
+
+// vehiculo.año = 2018
+// vehiculo.marca = "Toyota"
+// vehiculo.modelo = "Yaris"
+
+// pais = new Object()
+
+// pais.nombre = "Chile"
+// pais.localidad = "Sudamerica"
+// pais.habitantes = 1000000000
+
+
+// let elemento = prompt("ingrese un digito")
