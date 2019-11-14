748770d381313960aac309f4b6218d6b29bbc1ff
felipep-lagash <felipep@lagash.com>
2019-11-13 23:44:28 -0300

    Se mejoró funcionalidad de textoEnParentisis en funciones.js

--- Funciones/Ejercicio11/index.js
+++ Funciones/Ejercicio11/index.js
@@ -5,6 +5,7 @@ while(true) {
     let cadena = prompt('Ingrese texto por favor')
     if (esTexto(cadena)) {
         alert('Resultado: ' + textoEnParentisis(cadena))
+        break;
     }
     alert('Error: solo ingresa texto');
 }
--- Funciones/funciones.js
+++ Funciones/funciones.js
@@ -183,7 +183,7 @@ const textoEnParentisis = cadena => {
     for (let i = 0; i < cadena.length; i++) {
         if (cadena.charAt(i) == '(') {
             while(true) {
-                if (cadena.charAt(i+1) == ')' || (i+1) == cadena.length) {
+                if (cadena.charAt(i+1) == ')' || (i+1) == cadena.length || cadena.charAt(i+1) == '(') {
                     break;
                 }
                 textos += cadena.charAt(i+1);
@@ -192,6 +192,11 @@ const textoEnParentisis = cadena => {
             textos += ' ';
         }
         
+    }
+    console.log(textos.trim());
+    console.log(textos.trim().length);
+    if (textos.trim().length == 0) {
+        return 'No se encontro texto';
     }
     return textos;
 }
