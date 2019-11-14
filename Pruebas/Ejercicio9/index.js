07522c665ddf88f3b5af85383a21ab4b3cb819ef
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-14 15:07:17 -0300

    prueba con mas numeros y repetidos

--- Pruebas/Ejercicio9/index.js
+++ Pruebas/Ejercicio9/index.js
@@ -1,5 +1,5 @@
 import{ordenArray} from '../funciones.js'
 
-let arreglo =[23,57,12,2,99,64,734,212,44,55];
+let arreglo =[23,57,12,2,4,100, 2, 2, 2,99,64,734,212,44,55,99];
 
 alert(ordenArray(arreglo, "mayor"))
