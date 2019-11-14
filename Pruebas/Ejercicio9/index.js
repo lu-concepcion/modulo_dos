9ca5918068cb33e3744471f0ce89b27b4af63965
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-14 15:12:44 -0300

    agregar test numero negativo

--- Pruebas/Ejercicio9/index.js
+++ Pruebas/Ejercicio9/index.js
@@ -1,5 +1,5 @@
 import{ordenArray} from '../funciones.js'
 
-let arreglo =[23,57,12,2,4,100, 2, 2, 2,99,64,734,212,44,55,99];
+let arreglo =[23,57,12,2,4,100, 2, 2, 2,99,64,734,212,44,55,99,-100];
 
 alert(ordenArray(arreglo, "mayor"))
