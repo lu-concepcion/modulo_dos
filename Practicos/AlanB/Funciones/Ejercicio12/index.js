95636ec5e689907fab51d13cd9197aca8c07e7e0
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-14 16:33:17 -0300

    creacion ejercicio 12

---
+++ Practicos/AlanB/Funciones/Ejercicio12/index.js
@@ -0,0 +1,19 @@
+import{
+    mayoresQue
+}from'../Funciones.js'
+let personas=[
+    {
+        nombre:"Pedro",
+        fechaDeNacimiento: new Date(1996,8,11)
+    },
+    {
+        nombre:"Juan",
+        fechaDeNacimiento: new Date(2004,3,14)
+    },
+    {
+        nombre:"Diego",
+        fechaDeNacimiento: new Date(1987,5,21)
+    }
+]
+mayoresQue(personas,21)
+
