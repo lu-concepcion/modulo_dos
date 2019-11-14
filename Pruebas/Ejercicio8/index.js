0bcab80653ce7a790a9882a63f73e3b1ae7814be
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-14 14:59:57 -0300

    subiendo index del ejercicio 8

--- Pruebas/Ejercicio8/index.js
+++ Pruebas/Ejercicio8/index.js
@@ -1,17 +1,47 @@
-const encontrarElementoArray = (elemento,arreglo)=>{
-    for (let i = 0; i < arreglo.length; i++) {
-        if (elemento == arreglo[i]) return true;
-        
-    }
-    return false;
-}
+import{encontrarElementoArray,buscarEnArray, arrayFiltrado} from '../funciones.js'
 
-var arreglito = []
+/*var arreglito = []
 
 for (let i = 0; i < 6; i++) {
     arreglito[i] = prompt("ingrese algo");
     
 }
-var busqueda = prompt ("que desea busccar")
+var busqueda = prompt ("que desea buscar")
 
-alert( encontrarElementoArray(busqueda,arreglito))
+alert( encontrarElementoArray(busqueda,arreglito))*/
+
+
+
+let arrayPersonas = [
+    {
+    id: 1 ,
+    nombre: "Pedro",
+    edad: "14", 
+    },
+    {
+    id: 2,
+    nombre: "Pedro",
+    edad: "45", 
+    },
+    {
+    id: 3,
+    nombre: "Diego",
+    edad: "14", 
+    },
+    {
+    id: 4,
+    nombre: "Juana",
+    edad: "38", 
+    },
+    {
+    id: 5,
+    nombre: "Lewis",
+    edad: "68", 
+    }
+]
+
+var queBusco = prompt("¿Que desea buscar? ¿nombre o edad?");
+var valorBuscado = prompt("Agrega el criterio de busqueda");
+
+//console.log(buscarEnArray(arrayPersonas, queBusco, valorBuscado))
+console.log(arrayFiltrado(arrayPersonas, queBusco, valorBuscado))
