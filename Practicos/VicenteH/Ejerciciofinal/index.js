6e5c797b932ddb482d2b739fc8a09ad48c27179a
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-15 15:23:54 -0300

    creacion carpeta ejerciciofinal

---
+++ Practicos/VicenteH/Ejerciciofinal/index.js
@@ -0,0 +1,19 @@
+var funciones = require('./funciones')
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.post('/persona', function(request, response){
+  response.send({ "mensaje": "hola mundo"})
+});
+
+app.get('/persona/:id', function(request, response){
+  const { id } = request.params;
+  response.send({ "mensaje": `${id} hola`})
+});
+app.listen(3000, function () {
+  console.log('Simple node app for LU-CCP listening on port 3000!');
+});
+
