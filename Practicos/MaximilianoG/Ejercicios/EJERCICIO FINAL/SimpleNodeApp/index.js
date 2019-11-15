87f0d90b229921c22af370b94a04da11a7bab9d0
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-15 17:55:36 -0300

    Primer avance con Proyecto 1

--- Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/index.js
+++ Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/index.js
@@ -12,16 +12,22 @@ app.post('/persona', function(request, response){
   // body: {id: 1, nombre: hernan }
   // body = { id: 1, nombre: hernan }
   const { body }  = request;
+  //persona.prueba
+  //const {run, fechaNac, tieneProfesion, tieneVehiculo, vehiculoId, profesionId}=body
+  persona.guardar(body);
+  response.send(body);
 
-  response.send(body)
 });
 
-app.get('/persona', function(request, response){
+
+
+
+app.get('/persona/:run', function(request, response){
   // query mapea a un objeto los parámetros enviados en la ruta
   // ej: localhost:3000/persona?id=1&nombre=Hernan
   // query = { id: 1, nombre: hernan }
-  const { query }  = request;
-  response.send("hola mundo")
+  const { run }  = request.params;
+  response.send(persona.obtenerPersonas(run))
 });
 
 app.listen(3000, function () {
--- Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/repo/persona.js
+++ Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/repo/persona.js
@@ -1,1 +1,19 @@
 //generar datos de personas
+let personas = [];
+persona.rut;
+persona.nombre;
+
+const guardar = pers => {
+    personas.rut = pers.rut;
+    personas.nombre = pers.nombre;
+    personas.fechaNac = pers.fechaNac;
+    personas.edad = pers.fechaNac;
+}
+
+const obtenerPersona = (run) => {
+    return persona;
+}
+
+module.exports={
+    guardar, obtenerPersona
+}
