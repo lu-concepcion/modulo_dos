0eea221c7f911716e1994dafe7ebda75b9a9ccaf
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-15 01:25:23 -0300

    avance en funciones del ejercicio 8

--- Practicos/NataliaV/Funciones/Ejercicio8/index.js
+++ Practicos/NataliaV/Funciones/Ejercicio8/index.js
@@ -2,9 +2,26 @@ import{numeroaBuscar} from './funciones.js'
 let numero =[1,2,3,4,5,6,7,8,9,10];
 let n =parseInt(prompt("Ingrese un numero: "))
 let p;
-while(p=="s"){
+let arregloPersona={
+    nombre:'Natalia',
+    apellido:'Varela',
+    edad: 23,
+    sexo: 'femenino',
+ 
+    nombre:'Antonia',
+    apellido:'Yañez',
+    edad: 2,
+    sexo: 'femenino',
+
+    nombre:'Marco',
+    apellido:'Yañez',
+    edad: 23,
+    sexo: 'Maculino'
+
+};
+
 if(!numeroaBuscar(numero,n)){
     alert("no se encuentra "+n)
-    p=prompt("quiere ingresar otra vez s/n")
-    break;
-}}
+
+ 
+}
