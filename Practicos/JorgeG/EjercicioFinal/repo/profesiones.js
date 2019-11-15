38e531a29298c7ebd4ea9e69ab5782b84f08de12
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-15 16:12:54 -0300

    set de valores predefinidos listos

--- Practicos/JorgeG/EjercicioFinal/repo/profesiones.js
+++ Practicos/JorgeG/EjercicioFinal/repo/profesiones.js
@@ -1,1 +1,9 @@
 //generar datos de profesiones
+
+const {id,nombre} = profesion;
+const profesiones=[];
+profesiones[0]= {id:1,nombre:"Ingeniero"}=profesion;
+profesiones[1]= {id:2,nombre:"Medico"}=profesion;
+profesiones[2]= {id:3,nombre:"Arquitecto"}=profesion;
+profesiones[3]= {id:4,nombre:"Kinesiologo"}=profesion;
+profesiones[4]= {id:5,nombre:"Contador Auditor"}=profesion;
--- Practicos/JorgeG/EjercicioFinal/repo/vehiculos.js
+++ Practicos/JorgeG/EjercicioFinal/repo/vehiculos.js
@@ -1,1 +1,9 @@
 //generar datos de vehículos
+
+const {marca,modelo} = vehiculo;
+const vehiculos=[];
+vehiculos[0]= {marca:"Ford",modelo:"Mustang"}=vehiculo;
+vehiculos[1]= {marca:"Chevrolet",modelo:"Camaro"}=vehiculo;
+vehiculos[2]= {marca:"Toyota",modelo:"Supra"}=vehiculo;
+vehiculos[3]= {marca:"Suzuki",modelo:"Samurai"}=vehiculo;
+vehiculos[4]= {marca:"BMW",modelo:"m4"}=vehiculo;
