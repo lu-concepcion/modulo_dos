2f1c4bf5ed68afccc9665e8e3f4ab7f0462315ee
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-15 18:24:42 -0300

    se agrega logica Proyecto

--- Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/funciones.js
+++ Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/funciones.js
@@ -2,6 +2,32 @@ const testing = () => {
     return "test";
 }
 
+const ordenarArray = (elementos) =>{
+    let elementosOrdenados=[];
+
+    for (let i in elementos){
+        let numeroAOrdenar= elementos[i];
+        let contador =0;
+        let cont =0;
+        for (let j in elementos){
+            if (elementos[i]==elementos[j]){
+                cont ++;
+            }
+            if ((i!=j) && (elementos[j]<=elementos[i])){
+                contador++;
+            } 
+        }
+        elementosOrdenados[contador]=elementos[i];
+        if (cont>0){
+            for (let k=1; k<cont; k++){
+                elementosOrdenados[contador-k]=elementos[i];
+
+            }
+        }
+    }
+    return elementosOrdenados
+}
+
 module.exports = {
-    testing
+    testing, ordenarArray
 }
