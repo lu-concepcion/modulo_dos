1d87be8fbfc4848a07b4f09bcdcb00bc13eaf34b
nataliav-lagash <57206277+nataliav-lagash@users.noreply.github.com>
2019-11-15 15:07:34 -0300

    Update index.js
    avanse ejercicio 8

--- Practicos/NataliaV/Funciones/Ejercicio8/index.js
+++ Practicos/NataliaV/Funciones/Ejercicio8/index.js
@@ -3,22 +3,33 @@ let numero =[1,2,3,4,5,6,7,8,9,10];
 let n =parseInt(prompt("Ingrese un numero: "))
 let p;
 let arregloPersona=[{
+    id:1,
     nombre:'Natalia',
     apellido:'Varela',
-    edad: 23,
+    edad: '23',
     sexo: 'femenino'},
  
-    {nombre:'Antonia',
+    {
+    id:2,
+    nombre:'Antonia',
     apellido:'Yañez',
     edad: 2,
     sexo: 'femenino'},
 
-    {nombre:'Marco',
+    {
+    id:3,
+    nombre:'Marco',
     apellido:'Yañez',
-    edad: 23,
+    edad: 30,
     sexo: 'Maculino'}
 
 ];
+
+for (let i = 0; i < arregloPersona.length; i++) {
+    const element = arregloPersona;
+    console.log(arregloPersona[i]['nombre']);
+    
+}
 
 if(!numeroaBuscar(numero,n)){
     alert("no se encuentra "+n)
