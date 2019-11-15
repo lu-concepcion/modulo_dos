c5289f113b4f9a21af85922e1074c93e1be02531
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-15 17:01:37 -0300

    se añaden elementos a autos y profesiones

--- Practicos/AlanB/EjercicioFinal/repo/profesiones.js
+++ Practicos/AlanB/EjercicioFinal/repo/profesiones.js
@@ -1,1 +1,9 @@
-//generar datos de profesiones
+//generar datos de 
+let profesiones = [
+    {id:1,nombre:"ingeniero"},
+    {id:2,nombre:"profesor"},
+    {id:3,nombre:"medico"},
+    {id:4,nombre:"abogado"},
+    {id:5,nombre:"psicologo"},
+    {id:6,nombre:"dentista"}
+]
--- Practicos/AlanB/EjercicioFinal/repo/vehiculos.js
+++ Practicos/AlanB/EjercicioFinal/repo/vehiculos.js
@@ -1,1 +1,10 @@
 //generar datos de vehículos
+let vehiculos =[
+    {marca:"Audi",modelo:"A4"},
+    {marca:"Alfa Romeo",modelo:"Giulia"},
+    {marca:"Kia",modelo:"Morning"},
+    {marca:"Ford",modelo:"mustang"},
+    {marca:"Volkswagen",modelo:"golf"},
+    {marca:"Volvo",modelo:"V40"},
+
+]
