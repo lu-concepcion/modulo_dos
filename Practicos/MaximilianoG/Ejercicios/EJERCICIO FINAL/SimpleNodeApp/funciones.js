832b3deb5dc6a4d6f5c84fb16595123b90d11418
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-15 18:35:07 -0300

    Avance proyecto1

--- Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/funciones.js
+++ Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/funciones.js
@@ -2,32 +2,51 @@ const testing = () => {
     return "test";
 }
 
-const ordenarArray = (elementos) =>{
-    let elementosOrdenados=[];
+const codigoVerificador = (rut) => {
+    let aux = rut.split("-");
+    rut = aux[0];
+    aux = rut.split(".");
+    let aux2 = [];
 
-    for (let i in elementos){
-        let numeroAOrdenar= elementos[i];
-        let contador =0;
-        let cont =0;
-        for (let j in elementos){
-            if (elementos[i]==elementos[j]){
-                cont ++;
-            }
-            if ((i!=j) && (elementos[j]<=elementos[i])){
-                contador++;
-            } 
-        }
-        elementosOrdenados[contador]=elementos[i];
-        if (cont>0){
-            for (let k=1; k<cont; k++){
-                elementosOrdenados[contador-k]=elementos[i];
-
-            }
-        }
+    for (let i=0; i<aux.length; i++){
+        aux2 = aux2 + aux[i];
     }
-    return elementosOrdenados
+
+    let rutInvertido = "";
+    let x = aux2.length - 1;
+    while(x >= 0){
+        rutInvertido = rutInvertido + aux2.charAt(x);
+        x--;
+    }
+
+    const serie = [2,3,4,5,6,7,2,3,4,5,6,7];
+    let multiplicacion = 0;
+    for(let i=0; i < rutInvertido.length; i++){
+        multiplicacion = multiplicacion + (serie[i] * parseInt(rutInvertido.charAt(i)));
+    }
+
+    multiplicacion = multiplicacion % 11;
+    multiplicacion = 11 - multiplicacion;
+    switch(multiplicacion){
+        case 11:
+            return 0;
+        case 10:
+            return "K"
+        default:
+            return multiplicacion;
+    }
+}
+
+const buscarEnArray = (array, propiedad, valor ) => {
+    let objeto = {};
+    array.forEach(element => {
+        if(element[propiedad] == valor){
+            objeto = element;
+        }
+    });
+    return objeto;
 }
 
 module.exports = {
-    testing, ordenarArray
+    testing, codigoVerificador, buscarEnArray
 }
--- Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/repo/persona.js
+++ Practicos/MaximilianoG/Ejercicios/EJERCICIO FINAL/SimpleNodeApp/repo/persona.js
@@ -1,19 +1,39 @@
+const funciones = require('../funciones.js')
+const vehiculos = require("./vehiculos.js")
 //generar datos de personas
 let personas = [];
-persona.rut;
-persona.nombre;
 
-const guardar = pers => {
-    personas.rut = pers.rut;
-    personas.nombre = pers.nombre;
-    personas.fechaNac = pers.fechaNac;
-    personas.edad = pers.fechaNac;
+let vehiculo = {
+    marca: "",
+    modelo: ""
 }
 
-const obtenerPersona = (run) => {
-    return persona;
+let persona = {
+    rut: "",
+    dv: "",
+    fechaNac: "",
+    edad: 0,
+    tieneProfesion: false,
+    tieneVehiculo: false,
+    vehiculo
 }
 
-module.exports={
-    guardar, obtenerPersona
+const guardar = (per) => {
+    let {rut, fechaNac, edad, tieneProfesion, tieneVehiculo, idVehiculo} = per;
+    if(!isNaN(rut)){
+        persona.rut = rut;
+        persona.dv = funciones.codigoVerificador(persona.rut);
+    }
+    persona.fechaNac = fechaNac;
+    persona.edad = edad;
+    persona.tieneProfesion = tieneProfesion;
+    persona.tieneVehiculo = tieneVehiculo;
+    persona.vehiculo = funciones.buscarEnArray(vehiculos.vehiculos, "id", idVehiculo)
+    
+
+    personas.push(persona);
+}
+
+module.exports = {
+    guardar
 }
