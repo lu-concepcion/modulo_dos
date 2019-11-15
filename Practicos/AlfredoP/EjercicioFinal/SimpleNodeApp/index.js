6d8225e0279b1b24fce7c4f717f6ded4ee3b2d5a
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-11-15 15:48:41 -0300

    Se movio la caperta a EjercicioFinal

--- Practicos/AlfredoP/EjercicioFinal/SimpleNodeApp/index.js
+++ Practicos/AlfredoP/EjercicioFinal/SimpleNodeApp/index.js
@@ -1,8 +1,5 @@
 var funciones = require('./funciones')
-<<<<<<< HEAD
-=======
 var persona = require('./repo/persona')
->>>>>>> master
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -10,20 +7,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 app.post('/persona', function(request, response){
-<<<<<<< HEAD
-  response.send({ "mensaje": "hola mundo"})
-});
-
-app.get('/persona/:id', function(request, response){
-  const { id } = request.params;
-  response.send({ "mensaje": `${id} hola`})
-});
-
-app.listen(3000, function () {
-  console.log("Funciona..");
-})
-
-=======
   // body mapea a un objeto los parámetros enviados en el body del request
   // ej: localhost:3000/persona
   // body: {id: 1, nombre: hernan }
@@ -44,4 +27,3 @@ app.get('/persona', function(request, response){
 app.listen(3000, function () {
   console.log('Simple node app for LU-CCP listening on port 3000!');
 });
->>>>>>> master
