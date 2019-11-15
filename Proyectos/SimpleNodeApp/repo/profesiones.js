ca117832b20df4208a4b304976832d14ac6edfa5
melinag <melinag@lagash.com>
2019-11-15 17:02:50 -0300

    avande ejerciciofinal, se agrega arrays de profesiones y vehiculos

--- Proyectos/SimpleNodeApp/repo/profesiones.js
+++ Proyectos/SimpleNodeApp/repo/profesiones.js
@@ -1,1 +1,29 @@
 //generar datos de profesiones
+let profesiones = [
+
+    {
+    id: 1,
+    nombre:"ingeniero"
+    },
+    {
+    id: 2,
+    nombre:"profesor"
+    },
+    {
+    id: 3,
+    nombre: "Medico"
+    },
+    {
+    id: 1,
+    nombre:"abogado"
+    },
+
+    {
+    id: 1,
+    nombre:"bioquimico"
+    },
+    {
+    id: 1,
+    nombre:"arquitecto"
+    },
+]
--- Proyectos/SimpleNodeApp/repo/vehiculos.js
+++ Proyectos/SimpleNodeApp/repo/vehiculos.js
@@ -1,1 +1,19 @@
 //generar datos de vehículos
+let vehiculo = [
+   { marca: "audi",
+     modelo: " A4"
+    },
+    { marca: "toyota",
+     modelo: " corolla"
+    },
+    { marca: "fiat",
+     modelo: " 124"
+    },
+    { marca: "ford",
+     modelo: "munstang"
+    },
+    { marca: "Volkswagen",
+     modelo: " beetle"
+    },
+
+]
