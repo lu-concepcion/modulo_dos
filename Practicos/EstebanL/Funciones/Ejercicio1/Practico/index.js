82a15df19e731c9ab7795588fa08d2a9fe06d579
estebanl-lagash <estebanl@lagash.com>
2019-11-17 15:01:58 -0300

    Practica ejercicio 1 funciones

--- Practicos/EstebanL/Funciones/Ejercicio1/Practico/index.js
+++ Practicos/EstebanL/Funciones/Ejercicio1/Practico/index.js
@@ -1,12 +1,41 @@
 
-import {texto, mayuscula, minuscula} from '../funciones.js';
+import {texto, mayuscula, minuscula} from '../../funciones.js';
+
 
 const textoingresado = prompt ("ingrese texto o numero a analizar")
 
 
-    alert (texto(textoingresado))
-    alert (mayuscula(textoingresado))
-    alert (minuscula(textoingresado))
+if(texto(textoingresado)==true){
 
+    const menu = prompt ("Ingrese una opcion a entregar en el texto: \
+    1: Mayuscula, \
+    2: Minuscula")
 
+    switch(menu){
+
+        case "1": {
+
+            alert (mayuscula(textoingresado))
+            alert ("Gracias por utilizar nuestros sistemas")
+        break;
+    }   case "2": {
+
+            alert (minuscula(textoingresado))
+            alert ("Gracias por utilizar nuestros sistemas")
+        break;
+
+    }   default:{
+
+        alert ("Comando Desconocido")
+        alert ("Gracias por utilizar nuestros sistemas")
+        break;
+    }
+    }
+}else{
+
+    alert ("Gracias por utilizar nuestros sistemas")
+
+}
+    
+    
 
