b4e86b8e59905310471dbb0cb8d4bf4179bbf2c5
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-16 23:57:32 -0300

    Pruebas

--- Practicos/AndresD/Funciones/Ejercicio12/index.js
+++ Practicos/AndresD/Funciones/Ejercicio12/index.js
@@ -2,19 +2,19 @@ import {mayoresDeEdad} from '../funciones.js';
 
 const persona1 ={
 nombre:"juan",
-fechaNac : new Date("1959, 10, 15")
+fechaNac : new Date("1959/ 10/ 15")
 }
 const persona2 ={
 nombre:"benito",
-fechaNac : new Date("1980, 04, 10")
+fechaNac : new Date("1980/ 04/ 10")
 }
 const persona3 ={
 nombre:"menor",
-fechaNac : new Date("2005, 12, 17")
+fechaNac : new Date("2005/ 12/ 17")
 }
 const persona4 ={
 nombre:"andy",
-fechaNac : new Date("1995, 08, 21")
+fechaNac : new Date("1995/ 08/ 21")
 }
 
 let personas =[persona1, persona2, persona3, persona4];
