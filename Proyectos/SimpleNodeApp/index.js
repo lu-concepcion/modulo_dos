b2b76858083556114a2a00fbd4f02e69663e9963
melinag <melinag@lagash.com>
2019-11-18 16:39:28 -0300

    Avance ejercicio final, se añade funcion para obtener cursos y se modifica el index

--- Proyectos/SimpleNodeApp/index.js
+++ Proyectos/SimpleNodeApp/index.js
@@ -1,5 +1,6 @@
-var funciones = require('./funciones')
-var persona = require('./repo/persona')
+var funciones = require('./funciones');
+var curso = require('./repo/curso');
+var persona = require('./repo/persona');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -15,6 +16,15 @@ app.post('/persona', function(request, response){
   persona.guardarPersonas(body)
   response.send(body)
 });
+app.post('/curso', function(request, response){
+  // body mapea a un objeto los parámetros enviados en el body del request
+  // ej: localhost:3000/persona
+  // body: {id: 1, nombre: hernan }
+  // body = { id: 1, nombre: hernan }
+  const { body }  = request;
+  curso.guardarCurso(body)
+  response.send(body)
+});
 
 app.get('/persona/:run', function(request, response){
   // query mapea a un objeto los parámetros enviados en la ruta
@@ -23,7 +33,13 @@ app.get('/persona/:run', function(request, response){
   const { run }  = request.params;
   response.send(persona.obtenerPersonas(run))
 });
-
+app.get('/curso/:codigoCurso', function(request, response){
+  // query mapea a un objeto los parámetros enviados en la ruta
+  // ej: localhost:3000/persona?id=1&nombre=Hernan
+  // query = { id: 1, nombre: hernan }
+  const { codigoCurso }  = request.params;
+  response.send(curso.obtenerCurso(codigoCurso))
+});
 app.listen(3000, function () {
   console.log('Simple node app for LU-CCP listening on port 3000!');
 });
--- Proyectos/SimpleNodeApp/repo/curso.js
+++ Proyectos/SimpleNodeApp/repo/curso.js
@@ -1,24 +1,35 @@
 var persona = require('./persona.js')
 let cursos = [
      {codigoCurso: "codigo",
-      nombre: "prueba",
-      alumnos =[] }  ]
-      
-const guardarCursos = (body) =>{
+      nombreCurso: "prueba",
+      alumnos : [] }  ]
+
+const guardarCurso = (body) =>{
     let cursoNuevo = {};
-    let codigoCurso, nombre ;
+    let codigoCurso, nombreCurso ;
     let cantidadPropiedades = Object.keys(body).length
     
     for (let i = 0; i < cantidadPropiedades; i++) {
         if (Object.keys(body)[i]== "codigoCurso") {
             codigoCurso = Object.values(body)[i];
         }
-        if (Object.keys(body)[i]== "nombre") {
-            nombre = Object.values(body)[i];
+        if (Object.keys(body)[i]== "nombreCurso") {
+            nombreCurso = Object.values(body)[i];
         }
     }
  cursoNuevo.codigoCurso = codigoCurso
- cursoNuevo.nombre = nombre
+ cursoNuevo.nombreCurso = nombreCurso
  cursoNuevo.alumnos = [];
+ cursos.push(cursoNuevo)
 }
-
+const obtenerCurso = (codigoCurso) =>{
+    let filtrado={};
+    for (let i = 0; i < cursos.length; i++) {
+        if (Object.values(cursos[i])[0]==codigoCurso) {
+            filtrado = cursos[i];
+        }
+        
+    }
+    return filtrado;
+}
+module.exports = {obtenerCurso,guardarCurso}
