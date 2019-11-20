4903030aac87ee4e065baec2be49dd04a0fd2b05
jeremya-lagash <jeremya@lagash.com>
2019-11-20 15:01:41 -0300

    initial commit react

---
+++ Practicos/JeremyA/demo-react/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { timeout } from 'q';
+
+
+  class App extends React.Component{
+    constructor(promps){
+      super(promps);
+      this.state = {
+        value: 'Learn React'
+      };
+      this._varGlobal ='';
+      console.log('constructor')
+    }
+
+shouldComponentUpdate(nextProps, nextState){
+  const {value, compare} =this.state;
+
+  const should = nextState.value !== value ||
+  nextState.compare !== compare;
+  console.log('ShouldComponentUpdate', should)
+  return should;
+}
+
+    componentDidMount(){
+      console.log('componentDidMount')
+      setTimeout( () => {
+        this.setState({value: "Learn React Now!"});
+      }, 3000)
+      setTimeout( () => {
+        this._varGlobal = 'Please';
+        console.log('forceUpdate')
+        this.forceUpdate();
+      }, 4000)
+      setTimeout(() => {
+        console.log('setState compare')
+        this.setState({compare:''});
+      }, 5000)
+    }
+
+    render() {
+      console.log('render')
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {`${this.state.value} ${this._varGlobal}`}
+        </a>
+      </header>
+    </div>
+    );
+    }
+  }
+export default App;
