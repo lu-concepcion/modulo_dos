4db85ee06d6f3052cda632f85b576899e543c80b
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-20 15:07:26 -0300

    Se crea Carpeta de React (modulo 3)

---
+++ Practicos/MaximilianoG/Modulo 3/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Learn React',
+      compare: ''
+    };
+    this._varGlobal = '';
+    console.log('constructor')
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { value, compare } = this.state;
+    
+
+    const should = nextState.value !== value || nextState.compare !== compare;
+    console.log('shouldComponentUpdate', should)
+    return should;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    setTimeout(() => {
+      console.log('setState')
+      this.setState({value: 'Learn React Now!'});
+    }, 3000)
+    setTimeout(() => {
+      this._varGlobal = 'Please';
+      console.log('forceUpdate')
+      this.forceUpdate();
+    }, 4000)
+    setTimeout(() => {
+      console.log('setState compare')
+      this.setState({compare: ''});
+    }, 5000)
+  }
+
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  render() {
+    console.log('render')
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hola Maximiliano.!<code></code> 
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {`${this.state.value} ${this._varGlobal}`}
+          </a>
+        </header>
+      </div>
+    )
+  };
+}
+
+export default App;
